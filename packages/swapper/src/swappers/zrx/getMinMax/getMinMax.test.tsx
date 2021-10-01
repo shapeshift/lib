ecfbfaa4837d4ad3ed4e28bc99dd6abc1e9a964b
Jacob Cooley <10103480+toshiSat@users.noreply.github.com>
2021-10-01 16:38:58 -0600

    Update tests for getMinMax

---
+++ packages/swapper/src/swappers/zrx/getMinMax/getMinMax.test.tsx
@@ -0,0 +1,36 @@
+import { zrxService } from '../utils/zrxService'
+import { getMinMax } from './getMinMax'
+import { FOX, WETH, BTC } from '../utils/test-data/assets'
+import { getUsdRate } from '../utils/helpers/helpers'
+import { MAX_ZRX_TRADE } from '../utils/constants'
+
+const axios = jest.createMockFromModule('axios')
+//@ts-ignore
+axios.create = jest.fn(() => axios)
+jest.mock('../utils/helpers/helpers')
+jest.mock('../utils/zrxService')
+jest.mock('../getQuote/getQuote', () => ({
+  getZrxQuote: jest.fn()
+}))
+
+describe('getMinMax', () => {
+  it('returns minimum, maximum, and minimumPrice', async () => {
+    ;(zrxService.get as jest.Mock<unknown>).mockReturnValue(
+      Promise.resolve({ data: { price: '100' } })
+    )
+    ;(getUsdRate as jest.Mock<unknown>).mockReturnValue(1)
+    const minMax = await getMinMax({ sellAsset: FOX, buyAsset: WETH })
+    expect(minMax.minimum).toBe('1')
+    expect(minMax.maximum).toBe(MAX_ZRX_TRADE)
+    expect(minMax.minimumPrice).toBe('100')
+  })
+  it('fails on non eth asset', async () => {
+    ;(zrxService.get as jest.Mock<unknown>).mockReturnValue(
+      Promise.resolve({ data: { price: '100' } })
+    )
+    ;(getUsdRate as jest.Mock<unknown>).mockReturnValue(1)
+    await expect(getMinMax({ sellAsset: BTC, buyAsset: WETH })).rejects.toThrow(
+      'ZrxError:getMinMax - must be eth assets'
+    )
+  })
+})
--- packages/swapper/src/swappers/zrx/getMinMax/getMinMax.tsx
+++ packages/swapper/src/swappers/zrx/getMinMax/getMinMax.tsx
@@ -1,32 +1,21 @@
-import { ChainTypes, GetQuoteInput, QuoteResponse,MinMaxOutput } from '@shapeshiftoss/types'
+import { ChainTypes, GetQuoteInput, QuoteResponse, MinMaxOutput } from '@shapeshiftoss/types'
 import BigNumber from 'bignumber.js'
 import { MAX_ZRX_TRADE } from '../utils/constants'
 import { getUsdRate, normalizeAmount } from '../utils/helpers/helpers'
 import { zrxService } from '../utils/zrxService'
 import { ZrxError } from '../ZrxSwapper'
 
-export const getMinMax = async (input: GetQuoteInput): Promise<MinMaxOutput> => {
-  const { sellAsset, buyAsset, sellAmount } = input
-  if (!sellAmount) {
-    throw new ZrxError('sellAmount required')
-  }
+export const getMinMax = async (
+  input: Pick<GetQuoteInput, 'sellAsset' | 'buyAsset'>
+): Promise<MinMaxOutput> => {
+  const { sellAsset, buyAsset } = input
 
   if (sellAsset.chain !== ChainTypes.Ethereum || buyAsset.chain !== ChainTypes.Ethereum) {
-    throw new ZrxError('must be eth assets')
+    throw new ZrxError('getMinMax - must be eth assets')
   }
 
-  const buyToken = buyAsset.tokenId || buyAsset.symbol || buyAsset.chain
-  const sellToken = sellAsset.tokenId || sellAsset.symbol || sellAsset.chain
-  if (!buyToken) {
-    throw new ZrxError(
-      'ZrxSwapper:getQuote: One of buyAssetContract or buyAssetSymbol or buyAssetNetwork are required'
-    )
-  }
-  if (!sellToken) {
-    throw new ZrxError(
-      'ZrxSwapper:getQuote: One of sellAssetContract or sellAssetSymbol or sellAssetNetwork are required'
-    )
-  }
+  const buyToken = buyAsset.tokenId || buyAsset.symbol
+  const sellToken = sellAsset.tokenId || sellAsset.symbol
 
   const usdRate = await getUsdRate({
     symbol: sellAsset.symbol,
@@ -46,7 +35,6 @@ export const getMinMax = async (input: GetQuoteInput): Promise<MinMaxOutput> => 
       sellAmount: normalizeAmount(minimumWeiAmount.toString())
     }
   })
-
   const minimumPrice = new BigNumber(minimumPriceResult?.data?.price).toString()
 
   return {
