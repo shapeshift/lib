a8e1c5dc43f2d2fb8af97f2d095c24d45a0d0ae8
DaoDev <88341957+DaoDev44@users.noreply.github.com>
2021-10-22 10:51:46 -0600

    remove params comments

--- packages/swapper/src/api.ts
+++ packages/swapper/src/api.ts
@@ -22,33 +22,26 @@ export interface Swapper {
 
   /**
    * Get a Quote along with an unsigned transaction that can be signed and broadcast to execute the swap
-   * @param input
-   * @param wallet
    **/
   buildQuoteTx(args: BuildQuoteTxInput): Promise<Quote<ChainTypes, SwapperType>>
 
   /**
    * Get a basic quote (rate) for a trading pair
-   * @param input
    */
   getQuote(input: GetQuoteInput, wallet?: HDWallet): Promise<Quote<ChainTypes, SwapperType>>
 
   /**
    * Get a list of available assets based on the array of assets you send it
-   * @param assets
    */
   getAvailableAssets(assets: Asset[]): Asset[]
 
   /**
    * Get a boolean if the trade pair will work
-   * @param sellAsset
-   * @param buyAsset
    */
   canTradePair(sellAsset: Asset, buyAsset: Asset): boolean
 
   /**
    * Get the usd rate from either the assets symbol or tokenId
-   * @param input
    */
   getUsdRate(input: Pick<Asset, 'symbol' | 'tokenId'>): Promise<string>
 
@@ -59,21 +52,16 @@ export interface Swapper {
 
   /**
    * Get the minimum and maximum trade value of the sellAsset and buyAsset
-   * @param input
    */
   getMinMax(input: GetQuoteInput): Promise<MinMaxOutput>
 
   /**
    * Execute a quote built with buildQuoteTx by signing and broadcasting
-   * @param input
-   * @param wallet
    */
   executeQuote(args: ExecQuoteInput<ChainTypes, SwapperType>): Promise<ExecQuoteOutput>
 
   /**
    * Get a boolean if a quote needs approval
-   * @param quote
-   * @param wallet
    */
   approvalNeeded(args: ApprovalNeededInput): Promise<ApprovalNeededOutput>
 
