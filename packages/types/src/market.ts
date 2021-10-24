99e3f9c7319c1dc5767cf63e8ea828cc26f8b110
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2021-10-24 10:35:46 -0600

    feat: new types for market cap

---
+++ packages/types/src/market.ts
@@ -0,0 +1,68 @@
+import { ChainTypes } from '.'
+
+export type MarketData = {
+  price: string
+  marketCap: string
+  volume: string
+  changePercent24Hr: number
+}
+
+export enum HistoryTimeframe {
+  HOUR = '1H',
+  DAY = '24H',
+  WEEK = '1W',
+  MONTH = '1M',
+  YEAR = '1Y',
+  ALL = 'All'
+}
+
+export type HistoryData = {
+  price: number
+  date: string
+}
+
+export type PriceHistoryArgs = {
+  chain: ChainTypes
+  timeframe: HistoryTimeframe
+  tokenId?: string
+}
+
+export type MarketDataArgs = {
+  chain: ChainTypes
+  tokenId?: string
+}
+
+export type MarketDataType = (args: MarketDataArgs) => Promise<MarketData>
+
+export type PriceHistoryType = (args: PriceHistoryArgs) => Promise<HistoryData[]>
+
+export type GetByMarketCapType = () => Promise<CoinGeckoMarketCap[]>
+
+export type CoinGeckoMarketCap = {
+  id: string
+  symbol: string
+  name: string
+  image: string
+  current_price: number
+  market_cap: number
+  market_cap_rank: number
+  fully_diluted_valuation: number
+  total_volume: number
+  high_24h: number
+  low_24h: number
+  price_change_24h: number
+  price_change_percentage_24h: number
+  market_cap_change_24h: number
+  market_cap_change_percentage_24h: number
+  circulating_supply: number
+  total_supply: number
+  max_supply: number
+  ath: number
+  ath_change_percentage: number
+  ath_date: string
+  atl: number
+  atl_change_percentage: number
+  atl_date: string
+  roi: null
+  last_updated: string
+}
