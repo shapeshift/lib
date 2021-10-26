856c1127637c668bbda6da7f0e9a82e6254f7fb4
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2021-10-26 12:07:14 -0600

    feat: fromCAIP19 utility and tests

--- packages/caip/src/caip19/caip19.test.ts
+++ packages/caip/src/caip19/caip19.test.ts
@@ -1,5 +1,5 @@
 import { ChainTypes, ContractTypes, NetworkTypes } from '@shapeshiftoss/types'
-import { toCAIP19 } from './caip19'
+import { fromCAIP19, toCAIP19 } from './caip19'
 
 describe('caip19', () => {
   describe('toCAIP19', () => {
@@ -47,6 +47,62 @@ describe('caip19', () => {
       const network = NetworkTypes.TESTNET
       const result = toCAIP19({ chain, network })
       expect(result).toEqual('bip122:000000000933ea01ad0ee984209779ba/slip44:0')
+    })
+  })
+
+  describe('fromCAIP19', () => {
+    it('can return chain, network from eth caip19 on mainnet', () => {
+      const caip19 = 'eip155:1/slip44:60'
+      const { chain, network, contractType, tokenId } = fromCAIP19(caip19)
+      expect(chain).toEqual(ChainTypes.Ethereum)
+      expect(network).toEqual(NetworkTypes.MAINNET)
+      expect(contractType).toBeUndefined()
+      expect(tokenId).toBeUndefined()
+    })
+
+    it('can return chain, network from eth caip19 on ropsten', () => {
+      const caip19 = 'eip155:3/slip44:60'
+      const { chain, network, contractType, tokenId } = fromCAIP19(caip19)
+      expect(chain).toEqual(ChainTypes.Ethereum)
+      expect(network).toEqual(NetworkTypes.ETH_ROPSTEN)
+      expect(contractType).toBeUndefined()
+      expect(tokenId).toBeUndefined()
+    })
+
+    it('can return chain, network from bitcoin caip19 on mainnet', () => {
+      const caip19 = 'bip122:000000000019d6689c085ae165831e93/slip44:0'
+      const { chain, network, contractType, tokenId } = fromCAIP19(caip19)
+      expect(chain).toEqual(ChainTypes.Bitcoin)
+      expect(network).toEqual(NetworkTypes.MAINNET)
+      expect(contractType).toBeUndefined()
+      expect(tokenId).toBeUndefined()
+    })
+
+    it('can return chain, network from bitcoin caip19 on testnet', () => {
+      const caip19 = 'bip122:000000000933ea01ad0ee984209779ba/slip44:0'
+      const { chain, network, contractType, tokenId } = fromCAIP19(caip19)
+      expect(chain).toEqual(ChainTypes.Bitcoin)
+      expect(network).toEqual(NetworkTypes.TESTNET)
+      expect(contractType).toBeUndefined()
+      expect(tokenId).toBeUndefined()
+    })
+
+    it('can return chain, network, contractType, tokenId from FOX caip19 identifier on mainnet', () => {
+      const caip19 = 'eip155:1/erc20:0xc770eefad204b5180df6a14ee197d99d808ee52d'
+      const { chain, network, contractType, tokenId } = fromCAIP19(caip19)
+      expect(chain).toEqual(ChainTypes.Ethereum)
+      expect(network).toEqual(NetworkTypes.MAINNET)
+      expect(contractType).toEqual(ContractTypes.ERC20)
+      expect(tokenId).toEqual('0xc770eefad204b5180df6a14ee197d99d808ee52d')
+    })
+
+    it('can return chain, network, contractType, tokenId from FOX caip19 identifier on ropsten', () => {
+      const caip19 = 'eip155:3/erc20:0xc770eefad204b5180df6a14ee197d99d808ee52d'
+      const { chain, network, contractType, tokenId } = fromCAIP19(caip19)
+      expect(chain).toEqual(ChainTypes.Ethereum)
+      expect(network).toEqual(NetworkTypes.ETH_ROPSTEN)
+      expect(contractType).toEqual(ContractTypes.ERC20)
+      expect(tokenId).toEqual('0xc770eefad204b5180df6a14ee197d99d808ee52d')
     })
   })
 })
--- packages/caip/src/caip19/caip19.ts
+++ packages/caip/src/caip19/caip19.ts
@@ -1,4 +1,4 @@
-import { toCAIP2 } from './../caip2/caip2'
+import { fromCAIP2, toCAIP2 } from './../caip2/caip2'
 // https://github.com/ChainAgnostic/CAIPs/blob/master/CAIPs/caip-19.md
 
 import { ChainTypes, ContractTypes, NetworkTypes } from '@shapeshiftoss/types'
@@ -46,12 +46,88 @@ export const toCAIP19 = ({ chain, network, contractType, tokenId }: ToCAIP19Args
       } else {
         return `${caip2}/${AssetNamespace.Slip44}:${AssetReference.Ethereum}`
       }
-      break
     }
     case ChainTypes.Bitcoin: {
       return `${caip2}/${AssetNamespace.Slip44}:${AssetReference.Bitcoin}`
     }
   }
+}
+
+type FromCAIP19Return = {
+  chain: ChainTypes
+  network: NetworkTypes
+  contractType?: ContractTypes
+  tokenId?: string
+}
+
+export const fromCAIP19 = (caip19: string): FromCAIP19Return => {
+  const [caip2, namespaceAndReference] = caip19.split('/')
+  if (!(caip2 && namespaceAndReference)) {
+    throw new Error(
+      `fromCAIP19: error parsing caip19, caip2: ${caip2}, namespaceAndReference: ${namespaceAndReference}`
+    )
+  }
+
+  const [namespace, referenceString] = namespaceAndReference.split(':')
+  if (!(namespace && referenceString)) {
+    throw new Error(
+      `fromCAIP19: error parsing namespace and reference, namespace: ${namespace}, reference: ${referenceString}`
+    )
+  }
+
+  const reference = Number(referenceString)
+
+  const { chain, network } = fromCAIP2(caip2)
+
+  switch (chain) {
+    case ChainTypes.Bitcoin: {
+      switch (namespace) {
+        case AssetNamespace.Slip44: {
+          switch (reference) {
+            case AssetReference.Bitcoin: {
+              return { chain, network }
+            }
+            case AssetReference.Ethereum:
+            default: {
+              throw new Error(`fromCAIP19: invalid asset reference ${reference} on chain ${chain}`)
+            }
+          }
+        }
+        case AssetNamespace.ERC20:
+        case AssetNamespace.ERC721:
+        default: {
+          throw new Error(`fromCAIP19: invalid asset reference ${reference} on chain ${chain}`)
+        }
+      }
+    }
+    case ChainTypes.Ethereum: {
+      switch (namespace) {
+        case AssetNamespace.Slip44: {
+          switch (reference) {
+            case AssetReference.Ethereum: {
+              return { chain, network }
+            }
+            case AssetReference.Bitcoin:
+            default: {
+              throw new Error(`fromCAIP19: invalid asset reference ${reference} on chain ${chain}`)
+            }
+          }
+        }
+        case AssetNamespace.ERC20: {
+          const contractType = ContractTypes.ERC20
+          const tokenId = referenceString
+          return { chain, network, contractType, tokenId }
+        }
+        case AssetNamespace.ERC721: {
+          const contractType = ContractTypes.ERC721
+          const tokenId = referenceString
+          return { chain, network, contractType, tokenId }
+        }
+      }
+    }
+  }
+
+  throw new Error(`fromCAIP19: error parsing caip19: ${caip19}`)
 }
 
 export const CAIP19 = { AssetNamespace }
