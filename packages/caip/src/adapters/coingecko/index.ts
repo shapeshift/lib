3b93bac1305dee5efc073a6f564c7e354a8814c5
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2021-10-26 22:01:46 -0600

    feat: coingecko caip19 adapter to and from utilities and tests

--- packages/caip/src/adapters/coingecko/index.ts
+++ packages/caip/src/adapters/coingecko/index.ts
@@ -1,7 +1,31 @@
-const coingeckoToCAIP19 = (id: string): string => {
-  return ''
+import coingeckoBitcoinToCAIP19Map from './generated/bip122:000000000019d6689c085ae165831e93/coingeckoToCAIP19Map.json'
+import CAIP19ToCoingeckoBitcoinMap from './generated/bip122:000000000019d6689c085ae165831e93/CAIP19ToCoingeckoMap.json'
+import coingeckoEthereumToCAIP19Map from './generated/eip155:1/coingeckoToCAIP19Map.json'
+import CAIP19ToCoingeckoEthereumMap from './generated/eip155:1/CAIP19ToCoingeckoMap.json'
+
+// the compiler is too smart here, and can infer the keys of the json
+// it's not going to be ergonomic to expose these to the client, loosen it to string
+const coingeckoToCAIP19Map: Record<string, string> = {
+  ...coingeckoBitcoinToCAIP19Map,
+  ...coingeckoEthereumToCAIP19Map
+}
+const CAIP19ToCoingeckoMap: Record<string, string> = {
+  ...CAIP19ToCoingeckoBitcoinMap,
+  ...CAIP19ToCoingeckoEthereumMap
 }
 
-const CAIP19toCoingecko = (id: string): string => {
-  return ''
+export const coingeckoToCAIP19 = (id: string): string => {
+  const caip19 = coingeckoToCAIP19Map[id]
+  if (!caip19) {
+    throw new Error(`coingeckoToCAIP19: no CAIP19 found for ${id}`)
+  }
+  return caip19
+}
+
+export const CAIP19toCoingecko = (caip19: string): string => {
+  const id = CAIP19ToCoingeckoMap[caip19]
+  if (!caip19) {
+    throw new Error(`CAIP19toCoingecko: no id found for ${caip19}`)
+  }
+  return id
 }
