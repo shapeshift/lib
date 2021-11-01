6ddf538fcbc01351dcc855debc53d43f745e9d31
Reid Rankin <reidrankin@gmail.com>
2021-11-01 18:01:29 -0400

    refactor: simplify a bit (#179)

--- packages/chain-adapters/src/utils/utxoUtils.ts
+++ packages/chain-adapters/src/utils/utxoUtils.ts
@@ -1,6 +1,26 @@
-import { BTCInputScriptType } from '@shapeshiftoss/hdwallet-core'
+import { BTCInputScriptType, BTCOutputScriptType } from '@shapeshiftoss/hdwallet-core'
 import { Asset } from '@shapeshiftoss/types'
 import { BIP32Params, UtxoAccountType } from '@shapeshiftoss/types'
+
+/**
+ * Utility function to convert a BTCInputScriptType to the corresponding BTCOutputScriptType
+ * @param x a BTCInputScriptType
+ * @returns the corresponding BTCOutputScriptType
+ */
+export const toBtcOutputScriptType = (x: BTCInputScriptType) => {
+  switch (x) {
+    case BTCInputScriptType.SpendWitness:
+      return BTCOutputScriptType.PayToWitness
+    case BTCInputScriptType.SpendP2SHWitness:
+      return BTCOutputScriptType.PayToP2SHWitness
+    case BTCInputScriptType.SpendMultisig:
+      return BTCOutputScriptType.PayToMultisig
+    case BTCInputScriptType.SpendAddress:
+      return BTCOutputScriptType.PayToAddress
+    default:
+      throw new TypeError('scriptType')
+  }
+}
 
 /**
  * Utility function to get BIP32Params and scriptType for chain-adapter functions (getAddress, buildSendTransaction)
