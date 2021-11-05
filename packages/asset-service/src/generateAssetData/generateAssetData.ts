6518645200c88a7787def8a98a8a19ad030d90dd
theoboldfrazier <theoboldfrazier@gmail.com>
2021-11-05 17:12:09 -0600

    created erc20 extend file

--- packages/asset-service/src/generateAssetData/generateAssetData.ts
+++ packages/asset-service/src/generateAssetData/generateAssetData.ts
@@ -1,23 +1,10 @@
-import { caip19 } from '@shapeshiftoss/caip'
-import {
-  Asset,
-  BaseAsset,
-  ChainTypes,
-  ContractTypes,
-  NetworkTypes,
-  TokenAsset,
-  YearnApiVault
-} from '@shapeshiftoss/types'
-import axios, { AxiosInstance, AxiosResponse } from 'axios'
+import { Asset, BaseAsset, ChainTypes, NetworkTypes, TokenAsset } from '@shapeshiftoss/types'
 import fs from 'fs'
 import flatten from 'lodash/flatten'
 
 import { baseAssets } from './baseAssets'
 import { getTokens } from './ethTokens'
-
-const yearnAxios: AxiosInstance = axios.create({
-  baseURL: 'https://api.yearn.finance/v1'
-})
+import { extendErc20 } from './exetendErc20'
 
 const generateAssetData = async () => {
   const generatedAssetData = await Promise.all(
@@ -32,35 +19,13 @@ const generateAssetData = async () => {
     })
   )
 
-  const response: AxiosResponse = await yearnAxios.get(`/chains/1/vaults/all`)
-  const yearnVaults: YearnApiVault[] = response?.data as YearnApiVault[]
-  const generatedYearnAssets: TokenAsset[] = yearnVaults.map((vault: YearnApiVault) => {
-    if (!vault) return {} as TokenAsset
-    return {
-      color: '#FFFFFF',
-      contractType: ContractTypes.ERC20,
-      icon: vault.icon,
-      name: vault.name,
-      precision: vault.decimals,
-      receiveSupport: true,
-      secondaryColor: '#FFFFFF',
-      sendSupport: true,
-      symbol: vault.symbol,
-      tokenId: vault.address,
-      caip19: caip19.toCAIP19({
-        chain: ChainTypes.Ethereum,
-        network: NetworkTypes.MAINNET,
-        tokenId: vault.address,
-        contractType: ContractTypes.ERC20
-      })
-    } as TokenAsset
-  })
+  const extendedERC20Tokens = await extendErc20()
 
   const ethereumAsset = generatedAssetData.find(
     (asset: Asset) => asset.caip19 === 'eip155:1/slip44:60'
   )
-  if (ethereumAsset && generatedYearnAssets?.length) {
-    ethereumAsset.tokens = flatten([ethereumAsset.tokens as TokenAsset[], generatedYearnAssets])
+  if (ethereumAsset && extendedERC20Tokens?.length) {
+    ethereumAsset.tokens = flatten([ethereumAsset.tokens as TokenAsset[], extendedERC20Tokens])
   }
 
   await fs.promises.writeFile(
