4aa40be614c621ab272f006ae801e4bc75bca3be
theoboldfrazier <theoboldfrazier@gmail.com>
2021-11-08 17:17:07 -0700

    Updated logic for creating generated assets

--- packages/asset-service/src/generateAssetData/generateAssetData.ts
+++ packages/asset-service/src/generateAssetData/generateAssetData.ts
@@ -1,6 +1,5 @@
-import { Asset, BaseAsset, ChainTypes, NetworkTypes } from '@shapeshiftoss/types'
+import { BaseAsset, ChainTypes, NetworkTypes } from '@shapeshiftoss/types'
 import fs from 'fs'
-import flatten from 'lodash/flatten'
 
 import { baseAssets } from './baseAssets'
 import { getTokens } from './ethTokens'
@@ -10,23 +9,20 @@ const generateAssetData = async () => {
   const generatedAssetData = await Promise.all(
     baseAssets.map(async (baseAsset) => {
       if (baseAsset.chain === ChainTypes.Ethereum && baseAsset.network === NetworkTypes.MAINNET) {
-        const ethTokens = await getTokens()
-        const baseAssetWithTokens: BaseAsset = { ...baseAsset, tokens: ethTokens }
+        const [ethTokens, extendedERC20Tokens] = await Promise.all([
+          await getTokens(),
+          await extendErc20()
+        ])
+        const baseAssetWithTokens: BaseAsset = {
+          ...baseAsset,
+          tokens: ethTokens.concat(extendedERC20Tokens)
+        }
         return baseAssetWithTokens
       } else {
         return baseAsset
       }
     })
   )
-
-  const extendedERC20Tokens = await extendErc20()
-
-  const ethereumAsset = generatedAssetData.find(
-    (asset: Asset) => asset.caip19 === 'eip155:1/slip44:60'
-  )
-  if (ethereumAsset && extendedERC20Tokens?.length) {
-    ethereumAsset.tokens = flatten([ethereumAsset.tokens ?? [], extendedERC20Tokens])
-  }
 
   await fs.promises.writeFile(
     `./src/service/generatedAssetData.json`,
