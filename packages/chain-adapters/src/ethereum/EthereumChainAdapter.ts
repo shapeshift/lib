0055215d8eebaa0ce4cb6e15751daae70174ea58
Sean Luther <elmutt@gmail.com>
2021-12-03 12:47:01 -0700

    fix: find corresponding token transfer to use the right one for to address

--- packages/chain-adapters/src/ethereum/EthereumChainAdapter.ts
+++ packages/chain-adapters/src/ethereum/EthereumChainAdapter.ts
@@ -355,13 +355,20 @@ export class ChainAdapter implements IChainAdapter<ChainTypes.Ethereum> {
         }
 
         Object.entries(msg.send).forEach(([symbol, { totalValue, token }]) => {
+          const tokenTransferTo = msg?.tokenTransfers?.find((transfer) => {
+            return (
+              transfer.from === msg.vin[0]?.addresses?.[0] &&
+              transfer.token === token?.contract &&
+              transfer.value === totalValue
+            )
+          })?.to
           onMessage({
             ...baseTx,
             ...specificTx(symbol, totalValue, token),
             chain: ChainTypes.Ethereum,
             type: chainAdapters.TxType.Send,
             // For erc20 sends the vout address is the contract
-            to: msg?.tokenTransfers?.[0].to || msg.vout[0]?.addresses?.[0]
+            to: tokenTransferTo || msg.vout[0]?.addresses?.[0]
           })
         })
 
