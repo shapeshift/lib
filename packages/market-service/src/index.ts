7f93a6ab48e9b36f6992d25feadebf45c59683f4
theoboldfrazier <theoboldfrazier@gmail.com>
2021-12-07 11:42:00 -0700

    added comments for loop

--- packages/market-service/src/index.ts
+++ packages/market-service/src/index.ts
@@ -10,10 +10,15 @@ import { FindAllMarketArgs } from '@shapeshiftoss/types/src'
 
 import { CoinGeckoMarketService } from './coingecko/coingecko'
 
+// Order of this MarketProviders array constitutes the order of provders we will be checking first.
+// More reliable providers should be listed first.
 const MarketProviders = [new CoinGeckoMarketService()]
 
 export const findAll = async (args?: FindAllMarketArgs): Promise<MarketCapResult> => {
   let result: MarketCapResult | null = null
+  // Go through market providers listed above and look for market data for all assets.
+  // Once data is found, exist the loop and return result. If no data is found for any
+  // provider, throw an error.
   for (let i = 0; i < MarketProviders.length && !result; i++) {
     result = await MarketProviders[i].findAll(args)
   }
@@ -23,6 +28,7 @@ export const findAll = async (args?: FindAllMarketArgs): Promise<MarketCapResult
 
 export const findByCaip19 = async ({ caip19 }: MarketDataArgs) => {
   let result: MarketData | null = null
+  // Loop through market providers and look for asset market data. Once found, exit loop.
   for (let i = 0; i < MarketProviders.length && !result; i++) {
     result = await MarketProviders[i].findByCaip19({ caip19 })
   }
@@ -35,6 +41,7 @@ export const findPriceHistoryByCaip19: PriceHistoryType = async ({
   timeframe
 }: PriceHistoryArgs): Promise<HistoryData[]> => {
   let result: HistoryData[] | null = null
+  // Loop through market providers and look for asset price history data. Once found, exit loop.
   for (let i = 0; i < MarketProviders.length && !result; i++) {
     result = await MarketProviders[i].findPriceHistoryByCaip19({ caip19, timeframe })
   }
