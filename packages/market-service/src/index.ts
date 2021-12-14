3367cf7d78648e1717071080de398cef081dce9d
DaoDev <88341957+DaoDev44@users.noreply.github.com>
2021-12-14 11:25:11 -0700

    add error handling to market service manager

--- packages/market-service/src/index.ts
+++ packages/market-service/src/index.ts
@@ -6,13 +6,20 @@ import {
   PriceHistoryArgs,
   PriceHistoryType
 } from '@shapeshiftoss/types'
+import { Yearn } from '@yfi/sdk'
+import { JsonRpcProvider } from '@ethersproject/providers'
 import { FindAllMarketArgs } from '@shapeshiftoss/types/src'
 
 import { CoinGeckoMarketService } from './coingecko/coingecko'
+import { YearnMarketCapService } from './yearn/yearn'
+
+// YearnMarketCapService deps
+const provider = new JsonRpcProvider('https://dev-daemon.ethereum.shapeshift.com')
+const yearnSdk = new Yearn(1, { provider })
 
 // Order of this MarketProviders array constitutes the order of provders we will be checking first.
 // More reliable providers should be listed first.
-const MarketProviders = [new CoinGeckoMarketService()]
+const MarketProviders = [new CoinGeckoMarketService(), new YearnMarketCapService({ yearnSdk })]
 
 export const findAll = async (args?: FindAllMarketArgs): Promise<MarketCapResult> => {
   let result: MarketCapResult | null = null
@@ -20,7 +27,11 @@ export const findAll = async (args?: FindAllMarketArgs): Promise<MarketCapResult
   // Once data is found, exit the loop and return result. If no data is found for any
   // provider, throw an error.
   for (let i = 0; i < MarketProviders.length && !result; i++) {
-    result = await MarketProviders[i].findAll(args)
+    try {
+      result = await MarketProviders[i].findAll(args)
+    } catch (e) {
+      console.info(e)
+    }
   }
   if (!result) throw new Error('Cannot find market service provider for market data.')
   return result
@@ -30,7 +41,11 @@ export const findByCaip19 = async ({ caip19 }: MarketDataArgs) => {
   let result: MarketData | null = null
   // Loop through market providers and look for asset market data. Once found, exit loop.
   for (let i = 0; i < MarketProviders.length && !result; i++) {
-    result = await MarketProviders[i].findByCaip19({ caip19 })
+    try {
+      result = await MarketProviders[i].findByCaip19({ caip19 })
+    } catch (e) {
+      console.info(e)
+    }
   }
   if (!result) return null
   return result
