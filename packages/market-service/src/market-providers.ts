8b72876856a551ac1aba00918a7d27b155391069
theoboldfrazier <theoboldfrazier@gmail.com>
2021-12-16 14:47:33 -0700

    put in process.env

--- packages/market-service/src/market-providers.ts
+++ packages/market-service/src/market-providers.ts
@@ -4,7 +4,7 @@ import { Yearn } from '@yfi/sdk'
 import { CoinGeckoMarketService } from './coingecko/coingecko'
 import { YearnMarketCapService } from './yearn/yearn'
 // YearnMarketCapService deps
-const provider = new JsonRpcProvider('https://dev-daemon.ethereum.shapeshift.com')
+const provider = new JsonRpcProvider(process.env.REACT_APP_UNCHAINED_ETHEREUM_HTTP_URL)
 const yearnSdk = new Yearn(1, { provider })
 
 // Order of this MarketProviders array constitutes the order of provders we will be checking first.
