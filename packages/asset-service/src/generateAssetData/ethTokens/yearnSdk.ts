2b735af4b2ff6d96cf590ce39081f73ddf3060a3
theoboldfrazier <theoboldfrazier@gmail.com>
2022-01-24 16:12:49 -0700

    removed copy comment

--- packages/asset-service/src/generateAssetData/ethTokens/yearnSdk.ts
+++ packages/asset-service/src/generateAssetData/ethTokens/yearnSdk.ts
@@ -1,6 +1,5 @@
 import { JsonRpcProvider } from '@ethersproject/providers'
 import { Yearn } from '@yfi/sdk'
 
-// YearnMarketCapService deps
 const provider = new JsonRpcProvider(process.env.REACT_APP_UNCHAINED_ETHEREUM_HTTP_URL)
 export const yearnSdk = new Yearn(1, { provider })
