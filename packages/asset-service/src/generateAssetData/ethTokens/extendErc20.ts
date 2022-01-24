da73cb04a78592d36a60f6b02db5275cebe5bdc0
theoboldfrazier <theoboldfrazier@gmail.com>
2022-01-24 15:41:06 -0700

    getting underlying vault tokens

--- packages/asset-service/src/generateAssetData/ethTokens/extendErc20.ts
+++ packages/asset-service/src/generateAssetData/ethTokens/extendErc20.ts
@@ -7,6 +7,7 @@ import {
   TokenAsset
 } from '@shapeshiftoss/types'
 import { Token, Vault } from '@yfi/sdk'
+import fs from 'fs'
 import toLower from 'lodash/toLower'
 
 import { yearnSdk } from './yearnSdk'
@@ -44,6 +45,64 @@ export const getYearnVaults = async (): Promise<TokenAsset[]> => {
 export const getIronBankTokens = async (): Promise<TokenAsset[]> => {
   const ironBankTokens: Token[] = await yearnSdk.ironBank.tokens()
   return ironBankTokens.map((token: Token) => {
+    return {
+      color: '#FFFFFF',
+      contractType: ContractTypes.ERC20,
+      dataSource: AssetDataSource.YearnFinance,
+      icon: token.icon ?? '',
+      name: token.name,
+      precision: Number(token.decimals),
+      receiveSupport: true,
+      secondaryColor: '#FFFFFF',
+      sendSupport: true,
+      symbol: token.symbol,
+      tokenId: toLower(token.address),
+      caip2: caip2.toCAIP2({
+        chain: ChainTypes.Ethereum,
+        network: NetworkTypes.MAINNET
+      }),
+      caip19: caip19.toCAIP19({
+        chain: ChainTypes.Ethereum,
+        network: NetworkTypes.MAINNET,
+        tokenId: token.address,
+        contractType: ContractTypes.ERC20
+      })
+    }
+  })
+}
+
+export const getZapperTokens = async (): Promise<TokenAsset[]> => {
+  const zapperTokens: Token[] = await yearnSdk.tokens.supported()
+  return zapperTokens.map((token: Token) => {
+    return {
+      color: '#FFFFFF',
+      contractType: ContractTypes.ERC20,
+      dataSource: AssetDataSource.YearnFinance,
+      icon: token.icon ?? '',
+      name: token.name,
+      precision: Number(token.decimals),
+      receiveSupport: true,
+      secondaryColor: '#FFFFFF',
+      sendSupport: true,
+      symbol: token.symbol,
+      tokenId: toLower(token.address),
+      caip2: caip2.toCAIP2({
+        chain: ChainTypes.Ethereum,
+        network: NetworkTypes.MAINNET
+      }),
+      caip19: caip19.toCAIP19({
+        chain: ChainTypes.Ethereum,
+        network: NetworkTypes.MAINNET,
+        tokenId: token.address,
+        contractType: ContractTypes.ERC20
+      })
+    }
+  })
+}
+
+export const getUnderlyingVaultTokens = async (): Promise<TokenAsset[]> => {
+  const underlyingTokens: Token[] = await yearnSdk.vaults.tokens()
+  return underlyingTokens.map((token: Token) => {
     return {
       color: '#FFFFFF',
       contractType: ContractTypes.ERC20,
--- packages/asset-service/src/generateAssetData/generateAssetData.ts
+++ packages/asset-service/src/generateAssetData/generateAssetData.ts
@@ -1,22 +1,42 @@
 import { BaseAsset, ChainTypes, NetworkTypes } from '@shapeshiftoss/types'
 import fs from 'fs'
+import uniqBy from 'lodash/uniqBy'
 
 import { baseAssets } from './baseAssets'
 import { getTokens } from './ethTokens'
-import { getIronBankTokens, getYearnVaults } from './ethTokens/yearnErc20'
+import {
+  getIronBankTokens,
+  getUnderlyingVaultTokens,
+  getYearnVaults,
+  getZapperTokens
+} from './ethTokens/extendErc20'
 
 const generateAssetData = async () => {
   const generatedAssetData = await Promise.all(
     baseAssets.map(async (baseAsset) => {
       if (baseAsset.chain === ChainTypes.Ethereum && baseAsset.network === NetworkTypes.MAINNET) {
-        const [ethTokens, yearnVaults, ironBankTokens] = await Promise.all([
+        const [
+          ethTokens,
+          yearnVaults,
+          ironBankTokens,
+          zapperTokens,
+          underlyingTokens
+        ] = await Promise.all([
           await getTokens(),
           await getYearnVaults(),
-          await getIronBankTokens()
+          await getIronBankTokens(),
+          await getZapperTokens(),
+          await getUnderlyingVaultTokens()
         ])
+        const tokens = ethTokens
+          .concat(yearnVaults)
+          .concat(ironBankTokens)
+          .concat(zapperTokens)
+          .concat(underlyingTokens)
+        const uniqueTokens = uniqBy(tokens, 'caip19') // Remove dups
         const baseAssetWithTokens: BaseAsset = {
           ...baseAsset,
-          tokens: ethTokens.concat(yearnVaults).concat(ironBankTokens)
+          tokens: uniqueTokens
         }
         return baseAssetWithTokens
       } else {
