fcaef72bf624661baff5a81c181dabc53abbf133
Amitoj Singh <amitojsingh366@gmail.com>
2022-02-05 04:12:47 +0300

    feat: add expired prop on vaults

--- packages/investor-yearn/src/api/vaults.ts
+++ packages/investor-yearn/src/api/vaults.ts
@@ -14,22 +14,21 @@ export type SupportedYearnVault = {
   chain: ChainTypes
   provider: string
   type: string
+  expired: boolean
 }
 
 export const getSupportedVaults = async (): Promise<SupportedYearnVault[]> => {
   const vaults = await yearnSdk.vaults.get()
-
-  return vaults
-    .map((vault: Vault) => {
-      return {
-        vaultAddress: toLower(vault.address),
-        name: `${vault.name} ${vault.version}`,
-        symbol: vault.symbol,
-        tokenAddress: toLower(vault.token),
-        chain: ChainTypes.Ethereum,
-        provider: DefiProvider.Yearn,
-        type: DefiType.Vault,
-        expired: vault.metadata.depositsDisabled || !bnOrZero(vault.metadata.depositLimit).gt(0)
-      }
-    })
+  return vaults.map((vault: Vault) => {
+    return {
+      vaultAddress: toLower(vault.address),
+      name: `${vault.name} ${vault.version}`,
+      symbol: vault.symbol,
+      tokenAddress: toLower(vault.token),
+      chain: ChainTypes.Ethereum,
+      provider: DefiProvider.Yearn,
+      type: DefiType.Vault,
+      expired: !!vault.metadata.depositsDisabled || bnOrZero(vault.metadata.depositLimit).lte(0)
+    }
+  })
 }
