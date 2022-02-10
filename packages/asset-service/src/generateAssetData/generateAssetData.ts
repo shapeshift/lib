779dcd36fdd5c36401d4197496db7d2135a163ad
NeOMakinG <valentin.szczupak@prestashop.com>
2022-02-11 00:06:58 +0100

    fix: use recursive function at the end

--- packages/asset-service/src/generateAssetData/generateAssetData.ts
+++ packages/asset-service/src/generateAssetData/generateAssetData.ts
@@ -8,11 +8,25 @@ import { addTokensToEth } from './ethTokens'
 
 const blacklistedAssets: Array<string> = blacklist
 
+const filterBlacklistedAssets = <T extends BaseAsset | TokenAsset>(assets: T[]): T[] => {
+  const filteredAssets = filter(assets, (token) => {
+    if ('tokens' in token && token.tokens) {
+      token.tokens = filterBlacklistedAssets(token.tokens)
+    }
+
+    return !blacklistedAssets.includes(token.caip19)
+  })
+
+  return filteredAssets
+}
+
 const generateAssetData = async () => {
   const ethereum = await addTokensToEth()
   const osmosisAssets = await getOsmosisAssets()
 
   const generatedAssetData = [bitcoin, tBitcoin, ethereum, tEthereum, atom, ...osmosisAssets]
+
+  generatedAssetData = filterBlacklistedAssets(generatedAssetData)
 
   await fs.promises.writeFile(
     `./src/service/generatedAssetData.json`,
