a38c0164284a754965054922d9f08d5a810116d7
NeOMakinG <valentin.szczupak@prestashop.com>
2022-02-11 14:26:00 +0100

    fix: network type of mne in tests
    fix: fix test

--- packages/asset-service/src/generateAssetData/Utils.test.ts
+++ packages/asset-service/src/generateAssetData/Utils.test.ts
@@ -1,33 +1,22 @@
-import { AssetDataSource, BaseAsset, ChainTypes, NetworkTypes } from '@shapeshiftoss/types'
-import { filter } from 'lodash'
+import {
+  AssetDataSource,
+  BaseAsset,
+  ChainTypes,
+  ContractTypes,
+  NetworkTypes,
+  TokenAsset
+} from '@shapeshiftoss/types'
 
 import { filterBlacklistedAssets } from './generateAssetData'
 
 jest.mock(
   './blacklist.json',
-  () => ['eip155:3/slip44:60', 'eip155:1/erc20:0x426ca1ea2406c07d75db9585f22781c096e3d0e0'],
+  () => [
+    'bip122:000000000019d6689c085ae165831e93/slip44:0',
+    'eip155:1/erc20:0x426ca1ea2406c07d75db9585f22781c096e3d0e0'
+  ],
   { virtual: true }
 )
-
-const EthAsset: BaseAsset = {
-  caip19: 'eip155:3/slip44:60',
-  caip2: 'eip155:3',
-  chain: ChainTypes.Ethereum,
-  dataSource: AssetDataSource.CoinGecko,
-  network: NetworkTypes.ETH_ROPSTEN,
-  symbol: 'ETH',
-  name: 'Ropsten Testnet Ethereum',
-  precision: 18,
-  slip44: 1,
-  color: '#FFFFFF',
-  secondaryColor: '#FFFFFF',
-  icon: 'https://assets.coincap.io/assets/icons/eth@2x.png',
-  explorer: 'https://ropsten.etherscan.io/',
-  explorerTxLink: 'https://ropsten.etherscan.io/tx/',
-  explorerAddressLink: 'https://ropsten.etherscan.io/address/',
-  sendSupport: false,
-  receiveSupport: false
-}
 
 const BtcAsset: BaseAsset = {
   caip19: 'bip122:000000000019d6689c085ae165831e93/slip44:0',
@@ -49,24 +38,41 @@ const BtcAsset: BaseAsset = {
   receiveSupport: false
 }
 
-const MNEAsset: BaseAsset = {
+const MNEAsset: TokenAsset = {
+  tokenId: '0x426CA1eA2406c07d75Db9585F22781c096e3d0E0',
+  contractType: ContractTypes.ERC20,
   caip19: 'eip155:1/erc20:0x426ca1ea2406c07d75db9585f22781c096e3d0e0',
   caip2: 'eip155:1',
-  chain: ChainTypes.Bitcoin,
   dataSource: AssetDataSource.CoinGecko,
-  network: NetworkTypes.MAINNET,
   symbol: 'MNE',
   name: 'Minereum',
   precision: 8,
-  slip44: 0,
   color: '#FFFFFF',
   secondaryColor: '#FFFFFF',
   icon: 'https://rawcdn.githack.com/trustwallet/assets/master/blockchains/ethereum/assets/0x426ca1ea2406c07d75db9585f22781c096e3d0e0/logo.png',
-  explorer: 'https://etherscan.io',
-  explorerAddressLink: 'https://etherscan.io/address/',
-  explorerTxLink: 'https://etherscan.io/tx/',
   sendSupport: false,
   receiveSupport: false
+}
+
+const EthAsset: BaseAsset = {
+  caip19: 'eip155:3/slip44:60',
+  caip2: 'eip155:3',
+  chain: ChainTypes.Ethereum,
+  dataSource: AssetDataSource.CoinGecko,
+  network: NetworkTypes.ETH_ROPSTEN,
+  symbol: 'ETH',
+  name: 'Ropsten Testnet Ethereum',
+  precision: 18,
+  slip44: 1,
+  color: '#FFFFFF',
+  secondaryColor: '#FFFFFF',
+  icon: 'https://assets.coincap.io/assets/icons/eth@2x.png',
+  explorer: 'https://ropsten.etherscan.io/',
+  explorerTxLink: 'https://ropsten.etherscan.io/tx/',
+  explorerAddressLink: 'https://ropsten.etherscan.io/address/',
+  sendSupport: false,
+  receiveSupport: false,
+  tokens: [MNEAsset]
 }
 
 const assetList = [EthAsset, BtcAsset, MNEAsset]
@@ -74,7 +80,10 @@ const assetList = [EthAsset, BtcAsset, MNEAsset]
 describe('Utils', () => {
   describe('filterBlacklistedAssets', () => {
     it('should filter eth from the asset list', () => {
-      expect(filter(filterBlacklistedAssets(assetList))).toHaveLength(1)
+      const filteredAssetList = filterBlacklistedAssets(assetList)
+      expect(filteredAssetList).toHaveLength(1)
+      const ethFiltered = <BaseAsset>filteredAssetList[0]
+      expect(ethFiltered.tokens).toHaveLength(0)
     })
   })
 })
