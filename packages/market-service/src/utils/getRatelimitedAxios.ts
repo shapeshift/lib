3244f39867f5eea45068b65f058f129e5094039c
Amitoj Singh <amitojsingh366@gmail.com>
2022-02-12 20:52:08 +0300

    refactor: rate limits

--- packages/market-service/src/utils/getRatelimitedAxios.ts
+++ packages/market-service/src/utils/getRatelimitedAxios.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import rateLimit from 'axios-rate-limit'
 
-export const getRatelimitedAxios = (rps: number, perMilliseconds = 1000) => {
-  return rateLimit(axios.create(), { maxRequests: rps, perMilliseconds })
+// defaulting perMilliseconds to 60000 as both coingecko and coincap provide
+// xxx requests per minute (60000 ms) rate limits
+export const getRatelimitedAxios = (maxRequests: number, perMilliseconds = 60000) => {
+  return rateLimit(axios.create(), { maxRequests, perMilliseconds })
 }
