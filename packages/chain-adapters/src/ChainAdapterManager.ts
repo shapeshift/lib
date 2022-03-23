309afcbcb92923bc95626376dbba64def66ae6c8
DaoDev <88341957+DaoDev44@users.noreply.github.com>
2022-03-23 17:47:17 -0600

    revert chain adapter manager changes

--- packages/chain-adapters/src/ChainAdapterManager.ts
+++ packages/chain-adapters/src/ChainAdapterManager.ts
@@ -74,8 +74,6 @@ export class ChainAdapterManager {
       throw new Error('Parameter validation error')
     }
     this.supported.set(chain, factory)
-    const adapter = factory()
-    this.instances.set(chain, adapter)
   }
 
   removeChain<T extends ChainTypes>(chain: T): void {
@@ -86,7 +84,6 @@ export class ChainAdapterManager {
       throw new Error(`ChainAdapterManager: chain ${chain} not registered`)
     }
     this.supported.delete(chain)
-    this.instances.delete(chain)
   }
 
   getSupportedChains(): Array<ChainTypes> {
