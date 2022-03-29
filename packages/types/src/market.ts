3faa3cf3aa66cfb26baba05c9b594c43eddcfcf0
stackedQ <stackedQ@gmail.com>
2022-03-29 12:59:00 +0430

    fix: types improved

--- packages/types/src/market.ts
+++ packages/types/src/market.ts
@@ -41,27 +41,36 @@ export type MarketCapResult = {
   [k: string]: MarketData
 }
 
-export type SupportedFiatCurrencies =
-  | 'CNY'
-  | 'USD'
-  | 'EUR'
-  | 'JPY'
-  | 'GBP'
-  | 'KRW'
-  | 'INR'
-  | 'CAD'
-  | 'HKD'
-  | 'AUD'
-  | 'TWD'
-  | 'BRL'
-  | 'CHF'
-  | 'THB'
-  | 'MXN'
-  | 'RUB'
-  | 'SAR'
-  | 'SGD'
-  | 'ILS'
-  | 'IDR'
+/**
+ * stackedQ: We need the list in the frontend,
+ * and there's no way to convert an union type to an array,
+ * so the list is defined as an array of strings,
+ * but SupportedFiatCurrencies is an union of strings
+ */
+export const SupportedFiatCurrenciesList = [
+  'CNY',
+  'USD',
+  'EUR',
+  'JPY',
+  'GBP',
+  'KRW',
+  'INR',
+  'CAD',
+  'HKD',
+  'AUD',
+  'TWD',
+  'BRL',
+  'CHF',
+  'THB',
+  'MXN',
+  'RUB',
+  'SAR',
+  'SGD',
+  'ILS',
+  'IDR'
+] as const
+
+export type SupportedFiatCurrencies = typeof SupportedFiatCurrenciesList[number]
 
 export type FiatMarketDataArgs = {
   symbol: SupportedFiatCurrencies
