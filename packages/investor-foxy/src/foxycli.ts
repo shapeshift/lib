932f77ed4e84cc1d1695bd970dedeea370bacbc9
toshiSat <10103480+toshiSat@users.noreply.github.com>
2022-04-04 17:06:06 -0600

    chore: remove log

--- packages/investor-foxy/src/foxycli.ts
+++ packages/investor-foxy/src/foxycli.ts
@@ -54,9 +54,6 @@ const main = async (): Promise<void> => {
   const userAddress = await api.adapter.getAddress({ wallet })
   console.info('current user address ', userAddress)
 
-  const apy = api.apy()
-  console.info('apy', apy)
-
   const circulatingSupply = async () => {
     try {
       const supply = await api.tvl({ tokenContractAddress: foxyContractAddress })
