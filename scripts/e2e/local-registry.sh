65d911e4c868a81a451fca9fa26d1cfe741ce094
Grant Steuart <gmsteuart@gmail.com>
2022-05-02 05:34:52 -0500

    chore: init e2e release test

---
+++ scripts/e2e/local-registry.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+# @source https://github.com/nrwl/nx/blob/master/scripts/local-registry.sh
+# @source https://github.com/facebook/create-react-app/blob/v4.0.0/tasks/local-registry.sh
+
+set -x
+
+# TODO: set default if no arg
+verdaccio_config=$1
+
+default_verdaccio_package=verdaccio@^5.10.0
+local_registry_url="http://localhost:4873"
+original_npm_registry_url=`npm get registry`
+original_yarn_registry_url=`yarn config get registry`
+
+function startLocalRegistry {
+  tmp_registry_log=`mktemp`
+  echo "Starting Local Registry"
+  echo "Registry output file: $tmp_registry_log"
+  VERDACCIO_HANDLE_KILL_SIGNALS=true
+  # (yarn verdaccio --config ./.verdaccio/config.yml)
+  nohup npx $default_verdaccio_package --config $verdaccio_config &>$tmp_registry_log &
+  # Wait for Verdaccio to boot
+  grep -q 'http address' <(tail -f $tmp_registry_log)
+
+  echo "Setting registry to local registry"
+  npm config set registry $registry_url
+  yarn config set registry $registry_url
+}
+
+function stopLocalRegistry {
+  npm config delete registry
+  yarn config delete registry
+  CURRENT_NPM_REGISTRY=$(npm config get registry)
+  CURRENT_YARN_REGISTRY=$(yarn config get registry)
+
+  echo "Reverting registries"
+  echo "  > NPM:  $CURRENT_NPM_REGISTRY"
+  echo "  > YARN: $CURRENT_YARN_REIGSTRY"
+}
+
+if [[ $COMMAND == "clear" ]]; then
+  echo "Clearing Local Registry"
+  rm -rf ./build/local-registry/storage
+fi
+
+# Use following source to extend functionality:
+# @source: https://github.com/facebook/create-react-app/blob/v4.0.0/tasks/publish.sh
+function publishToLocalRegistry {
+	echo "Publishing to Local Registry"
+  # Remove untracked files from the working tree
+  # -d to recurse directories since no path is specified
+  # -f force
+	git clean -df
+  yarn release --dry-run
+  # --yes skip all confirmation prompts
+  # ./node_modules/.bin/lerna publish --yes --force-publish=* --no-git-tag-version --no-commit-hooks --no-push --exact --dist-tag=latest
+}
---
+++ scripts/e2e/release.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# @source https://github.com/facebook/create-react-app/blob/v4.0.0/tasks/e2e-simple.sh
+
+set -x
+
+root_path=$PWD
+
+
+# Load functions for working with local NPM registry (Verdaccio)
+source ./scripts/e2e/local-registry.sh
+
+function cleanup {
+  echo "Cleaning up."
+  stopLocalRegistry
+}
+
+# Error messages are redirected to stderr
+function handle_error {
+  echo "$(basename $0): ERROR! An error was encountered executing line $1." 1>&2;
+  cleanup
+  echo "Exiting with error." 1>&2;
+  exit 1
+}
+
+function handle_exit {
+  cleanup
+  echo "Exiting without error." 1>&2;
+  exit
+}
+
+# Exit the script with a helpful error message when any error is encountered
+trap 'set +x; handle_error $LINENO $BASH_COMMAND' ERR
+
+# Cleanup before exit on any termination signal
+trap 'set +x; handle_exit' SIGQUIT SIGTERM SIGINT SIGKILL SIGHUP
+
+# Bootstrap monorepo
+yarn
+
+startLocalRegistry ./.verdaccio/config.yml
+
+yarn lint
+yarn build
+# TODO: test dev encironment
+
+# Run tests with CI flag
+CI=true yarn test
+
+# Publish monorepo to local registry
+publishToLocalRegistry
+
+# Cleanup
+cleanup
