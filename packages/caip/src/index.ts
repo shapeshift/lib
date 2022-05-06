e08e756cae8ba9a2ca5db8425071e61b0a3490f4
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-05-07 09:47:12 +1000

    chore(caip): remove caip vernacular
    BREAKING CHANGE

--- packages/caip/src/index.ts
+++ packages/caip/src/index.ts
@@ -1,10 +1,10 @@
+import * as accountId from './accountId/accountId'
 import * as adapters from './adapters'
+import * as assetId from './assetId/assetId'
 // TODO: These all go away on the caip breaking change PR
-import * as caip2 from './caip2/caip2'
-import * as caip10 from './caip10/caip10'
-import * as caip19 from './caip19/caip19'
-export { adapters, caip2, caip19, caip10 }
+import * as chainId from './chainId/chainId'
+export { adapters, chainId, assetId, accountId }
 // ENDTODO
-export * from './caip2/caip2'
-export * from './caip10/caip10'
-export * from './caip19/caip19'
+export * from './chainId/chainId'
+export * from './accountId/accountId'
+export * from './assetId/assetId'
