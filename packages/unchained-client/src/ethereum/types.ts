bb0e78b3923a2ebbd00b489da88242dbc4ecb6b5
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2022-05-19 18:37:17 -0600

    remove internal tx local type

--- packages/unchained-client/src/ethereum/types.ts
+++ packages/unchained-client/src/ethereum/types.ts
@@ -1,23 +1,6 @@
 import { EthereumTx } from '../generated/ethereum'
 import { StandardTx, StandardTxMetadata } from '../types'
 
-export interface InternalTx {
-  blockNumber: string
-  timeStamp: string
-  hash: string
-  from: string
-  to: string
-  value: string
-  contractAddress: string
-  input: string
-  type: string
-  gas: string
-  gasUsed: string
-  traceId: string
-  isError: string
-  errCode: string
-}
-
 export interface ParsedTx extends StandardTx {
   data?: StandardTxMetadata
 }
