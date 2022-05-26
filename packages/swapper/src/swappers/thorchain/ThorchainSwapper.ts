ea01e12872bda86de27c6aa997fcfcbad0b9b14d
Sean Luther <elmutt@gmail.com>
2022-05-25 20:03:43 -0600

    remove unused

--- packages/swapper/src/swappers/thorchain/ThorchainSwapper.ts
+++ packages/swapper/src/swappers/thorchain/ThorchainSwapper.ts
@@ -9,49 +9,16 @@ import {
   SupportedChainIds,
   SwapperType
 } from '@shapeshiftoss/types'
-import axios from 'axios'
 
 import { Swapper, Trade, TradeQuote } from '../../api'
-import { midgardService } from './utils'
-
 export class ThorchainSwapper implements Swapper {
-  // Map our caip chain ids to symbols understood by thorchain
-  private supportedChainIdToSymbol = {
-    'eip155:1': 'ETH'
-  }
-
-  // ETH, BTC
-  private supportedChainIds = Object.keys(this.supportedChainIdToSymbol)
-
-  // Populated by initialize()
-  // private supportedAssetIds = []
-
   getType() {
     return SwapperType.Thorchain
   }
 
-  async initialize() {
-    console.log('supportedChainIds!', this.supportedChainIds)
-
-    try {
-      console.log('fuck!!')
-      const res = await axios.get(
-        'https://thor-midgard.cointainers.prod.chiefhappinessofficerellie.org/'
-      )
-      console.log('res is', res)
-      const poolsResponse = await midgardService.get('/v2/pools')
-      console.log('poolsResponse!', poolsResponse)
-    } catch (e) {
-      console.log('e is', { e })
-    }
-
-    // assetId: toAssetId({
-    //   chain: ChainTypes.Ethereum,
-    //   network: NetworkTypes.MAINNET,
-    //   assetNamespace: 'erc20',
-    //   assetReference: token.address
-    // })
-  }
+  // TODO populate supported assets from midgard
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  async initialize() {}
 
   getUsdRate(input: Pick<Asset, 'symbol' | 'assetId'>): Promise<string> {
     console.info(input)
