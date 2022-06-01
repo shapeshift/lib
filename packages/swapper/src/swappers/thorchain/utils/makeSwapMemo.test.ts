4e06dd56cd880fdebde18791594231991c7f9362
Sean Luther <elmutt@gmail.com>
2022-06-01 14:17:04 -0600

    fix test verbage

--- packages/swapper/src/swappers/thorchain/utils/makeSwapMemo.test.ts
+++ packages/swapper/src/swappers/thorchain/utils/makeSwapMemo.test.ts
@@ -1,7 +1,7 @@
 import { makeSwapMemo } from './makeSwapMemo'
 
 describe('makeSwapMemo', () => {
-  it('make a trade to usdc memo with', () => {
+  it('should make a trade to usdc memo', () => {
     const memo = makeSwapMemo({
       buyAssetId: 'eip155:1/erc20:0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
       destinationAddress: '0x8a65ac0E23F31979db06Ec62Af62b132a6dF4741',
@@ -11,7 +11,7 @@ describe('makeSwapMemo', () => {
       's:ETH.USDC-9D4A2E9EB0CE3606EB48:0x8a65ac0E23F31979db06Ec62Af62b132a6dF4741:420'
     )
   })
-  it('make a trade to eth memo', () => {
+  it('should make a trade to eth memo', () => {
     const memo = makeSwapMemo({
       buyAssetId: 'eip155:1/slip44:60',
       destinationAddress: '0x8a65ac0E23F31979db06Ec62Af62b132a6dF4741',
@@ -19,7 +19,7 @@ describe('makeSwapMemo', () => {
     })
     expect(memo).toEqual('s:ETH.ETH:0x8a65ac0E23F31979db06Ec62Af62b132a6dF4741:420')
   })
-  it('make a trade to btc memo', () => {
+  it('should make a trade to btc memo', () => {
     const memo = makeSwapMemo({
       buyAssetId: 'bip122:000000000019d6689c085ae165831e93/slip44:0',
       destinationAddress: 'bc1qkw9g3tgv6m2gwc4x4hvdefcwt0uxeedfgag27h',
