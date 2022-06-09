f0687270d9a5dc39b8db0234399de572264d38e2
DaoDev <88341957+DaoDev44@users.noreply.github.com>
2022-06-09 10:16:09 -0600

    add try/catch

--- packages/swapper/src/swappers/thorchain/utils/ethereum/utils/getThorTxData.ts
+++ packages/swapper/src/swappers/thorchain/utils/ethereum/utils/getThorTxData.ts
@@ -26,63 +26,68 @@ export const getThorTxInfo = async ({
   destinationAddress: string
   isErc20Trade: boolean
 }) => {
-  const { assetReference } = fromAssetId(sellAsset.assetId)
+  try {
+    const { assetReference } = fromAssetId(sellAsset.assetId)
 
-  const { data: inboundAddresses } = await thorService.get<InboundResponse[]>(
-    `${deps.midgardUrl}/thorchain/inbound_addresses`
-  )
+    const { data: inboundAddresses } = await thorService.get<InboundResponse[]>(
+      `${deps.midgardUrl}/thorchain/inbound_addresses`
+    )
 
-  const ethInboundAddresses = inboundAddresses.find((inbound) => inbound.chain === 'ETH')
+    const ethInboundAddresses = inboundAddresses.find((inbound) => inbound.chain === 'ETH')
 
-  const vault = ethInboundAddresses?.address
-  const router = ethInboundAddresses?.router
+    const vault = ethInboundAddresses?.address
+    const router = ethInboundAddresses?.router
 
-  if (!vault || !router)
-    throw new SwapError(`[getPriceRatio]: router or vault found for ETH`, {
-      code: SwapErrorTypes.RESPONSE_ERROR,
-      details: { inboundAddresses }
+    if (!vault || !router)
+      throw new SwapError(`[getPriceRatio]: router or vault found for ETH`, {
+        code: SwapErrorTypes.RESPONSE_ERROR,
+        details: { inboundAddresses }
+      })
+
+    const priceRatio = await getPriceRatio(deps, {
+      buyAssetId: buyAsset.assetId,
+      sellAssetId: sellAsset.assetId
     })
 
-  const priceRatio = await getPriceRatio(deps, {
-    buyAssetId: buyAsset.assetId,
-    sellAssetId: sellAsset.assetId
-  })
+    const expectedBuyAmount = toBaseUnit(
+      fromBaseUnit(bnOrZero(sellAmount).dividedBy(priceRatio), sellAsset.precision),
+      buyAsset.precision
+    )
 
-  const expectedBuyAmount = toBaseUnit(
-    fromBaseUnit(bnOrZero(sellAmount).dividedBy(priceRatio), sellAsset.precision),
-    buyAsset.precision
-  )
+    if (
+      !bnOrZero(expectedBuyAmount).gte(0) ||
+      !(bnOrZero(slippageTolerance).gte(0) && bnOrZero(slippageTolerance).lte(1))
+    )
+      throw new SwapError('[makeTradeTx]: bad expected buy amount or bad slippage tolerance', {
+        code: SwapErrorTypes.BUILD_TRADE_FAILED
+      })
 
-  if (
-    !bnOrZero(expectedBuyAmount).gte(0) ||
-    !(bnOrZero(slippageTolerance).gte(0) && bnOrZero(slippageTolerance).lte(1))
-  )
-    throw new SwapError('[makeTradeTx]: bad expected buy amount or bad slippage tolerance', {
-      code: SwapErrorTypes.BUILD_TRADE_FAILED
+    const limit = bnOrZero(expectedBuyAmount)
+      .times(bn(1).minus(slippageTolerance))
+      .decimalPlaces(0)
+      .toString()
+
+    const memo = makeSwapMemo({
+      buyAssetId: buyAsset.assetId,
+      destinationAddress,
+      limit
     })
 
-  const limit = bnOrZero(expectedBuyAmount)
-    .times(bn(1).minus(slippageTolerance))
-    .decimalPlaces(0)
-    .toString()
+    const data = await deposit(
+      router,
+      vault,
+      isErc20Trade ? assetReference : '0x0000000000000000000000000000000000000000',
+      sellAmount,
+      memo
+    )
 
-  const memo = makeSwapMemo({
-    buyAssetId: buyAsset.assetId,
-    destinationAddress,
-    limit
-  })
-
-  const data = await deposit(
-    router,
-    vault,
-    isErc20Trade ? assetReference : '0x0000000000000000000000000000000000000000',
-    sellAmount,
-    memo
-  )
-
-  return {
-    data,
-    memo,
-    router
+    return {
+      data,
+      memo,
+      router
+    }
+  } catch (e) {
+    if (e instanceof SwapError) throw e
+    throw new SwapError('[getThorTxInfo]', { cause: e, code: SwapErrorTypes.TRADE_QUOTE_FAILED })
   }
 }
--- packages/swapper/src/swappers/thorchain/utils/txFeeHelpers/ethTxFees/getEthTxFees.ts
+++ packages/swapper/src/swappers/thorchain/utils/txFeeHelpers/ethTxFees/getEthTxFees.ts
@@ -2,7 +2,7 @@ import { AssetReference } from '@shapeshiftoss/caip'
 import { ChainAdapterManager } from '@shapeshiftoss/chain-adapters'
 import { Asset, chainAdapters, ChainTypes } from '@shapeshiftoss/types'
 
-import { QuoteFeeData } from '../../../../../api'
+import { QuoteFeeData, SwapError, SwapErrorTypes } from '../../../../../api'
 import { bnOrZero } from '../../../../utils/bignumber'
 import { APPROVAL_GAS_LIMIT } from '../../../../utils/constants'
 import { ThorchainSwapperDeps } from '../../../types'
@@ -27,27 +27,33 @@ export const getEthTxFees = async ({
   adapterManager: ChainAdapterManager
   receiveAddress: string
 }): Promise<QuoteFeeData<'eip155:1'>> => {
-  const adapter = adapterManager.byChainId('eip155:1')
-  const feeDataOptions = await adapter.getFeeData({
-    to: router,
-    value: sellAmount,
-    chainSpecific: { from: receiveAddress, contractData: data }
-  })
+  try {
+    const adapter = adapterManager.byChainId('eip155:1')
+    // TODO(ryankk): add falling back on hard coded gas Limit * new chain adapter method if it fails
+    const feeDataOptions = await adapter.getFeeData({
+      to: router,
+      value: sellAmount,
+      chainSpecific: { from: receiveAddress, contractData: data }
+    })
 
-  const feeData = feeDataOptions['fast'] as chainAdapters.FeeData<ChainTypes.Ethereum>
-  const tradeFee = await estimateTradeFee(deps, buyAsset.assetId)
+    const feeData = feeDataOptions['fast'] as chainAdapters.FeeData<ChainTypes.Ethereum>
+    const tradeFee = await estimateTradeFee(deps, buyAsset.assetId)
 
-  return {
-    fee: feeData.txFee,
-    chainSpecific: {
-      estimatedGas: feeData.chainSpecific.gasLimit,
-      gasPrice: feeData.chainSpecific.gasPrice,
-      approvalFee:
-        sellAssetReference &&
-        bnOrZero(APPROVAL_GAS_LIMIT)
-          .multipliedBy(bnOrZero(feeData.chainSpecific.gasPrice))
-          .toString()
-    },
-    tradeFee
+    return {
+      fee: feeData.txFee,
+      chainSpecific: {
+        estimatedGas: feeData.chainSpecific.gasLimit,
+        gasPrice: feeData.chainSpecific.gasPrice,
+        approvalFee:
+          sellAssetReference &&
+          bnOrZero(APPROVAL_GAS_LIMIT)
+            .multipliedBy(bnOrZero(feeData.chainSpecific.gasPrice))
+            .toString()
+      },
+      tradeFee
+    }
+  } catch (e) {
+    if (e instanceof SwapError) throw e
+    throw new SwapError('[getThorTxInfo]', { cause: e, code: SwapErrorTypes.TRADE_QUOTE_FAILED })
   }
 }
