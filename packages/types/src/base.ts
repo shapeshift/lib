8d4399f1d3f49af5825e189e3ce05613334c6a69
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-06-12 16:50:11 +1000

    chore: remove NetworkTypes

--- packages/types/src/base.ts
+++ packages/types/src/base.ts
@@ -15,17 +15,6 @@ export enum KnownChainIds {
   OsmosisMainnet = 'cosmos:osmosis-1'
 }
 
-export enum NetworkTypes {
-  MAINNET = 'MAINNET',
-  TESTNET = 'TESTNET', // BTC, LTC, etc...
-  ETH_ROPSTEN = 'ETH_ROPSTEN',
-  ETH_RINKEBY = 'ETH_RINKEBY',
-  COSMOSHUB_MAINNET = 'COSMOSHUB_MAINNET',
-  COSMOSHUB_VEGA = 'COSMOSHUB_VEGA',
-  OSMOSIS_MAINNET = 'OSMOSIS_MAINNET',
-  OSMOSIS_TESTNET = 'OSMOSIS_TESTNET'
-}
-
 export enum WithdrawType {
   DELAYED,
   INSTANT
@@ -50,8 +39,6 @@ export type Asset = {
   chainId: string
   description?: string
   isTrustedDescription?: boolean
-  /** @deprecated: do not use. This will be removed once consumers have handled it */
-  network?: NetworkTypes
   symbol: string
   name: string
   precision: number
