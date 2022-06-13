b5b6642c3a262d7b859ed6a8e2ef7b9ec5aa4723
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-06-14 07:02:46 +1000

    chore: simplify chainIdToCoingeckoAssetPlatform and fix logic

--- packages/asset-service/src/utils.ts
+++ packages/asset-service/src/utils.ts
@@ -1,32 +1,27 @@
-import {
-  CHAIN_NAMESPACE,
-  CHAIN_REFERENCE,
-  ChainId,
-  ChainNamespace,
-  ChainReference,
-  fromChainId
-} from '@shapeshiftoss/caip'
+import { CHAIN_NAMESPACE, CHAIN_REFERENCE, ChainId, fromChainId } from '@shapeshiftoss/caip'
 
 // https://www.coingecko.com/en/api/documentation - See asset_platforms
-export const chainPartsToCoingeckoAssetPlatform = (
-  chainNamespace: ChainNamespace,
-  chainReference?: ChainReference
-): string => {
-  return (() => {
-    switch (chainNamespace) {
-      case CHAIN_NAMESPACE.Ethereum:
-        return 'ethereum'
-      case CHAIN_NAMESPACE.Cosmos:
-        return chainReference === CHAIN_REFERENCE.CosmosHubMainnet ? 'cosmos' : 'osmosis'
-      default:
-        throw new Error(
-          `chainNamespace ${chainNamespace}, chainReference ${chainReference} not supported.`
-        )
-    }
-  })()
-}
-
 export const chainIdToCoingeckoAssetPlatform = (chainId: ChainId): string => {
   const { chainNamespace, chainReference } = fromChainId(chainId)
-  return chainPartsToCoingeckoAssetPlatform(chainNamespace, chainReference)
+  switch (chainNamespace) {
+    case CHAIN_NAMESPACE.Ethereum:
+      return 'ethereum'
+    case CHAIN_NAMESPACE.Cosmos:
+      switch (chainReference) {
+        case CHAIN_REFERENCE.CosmosHubMainnet:
+          return 'cosmos'
+        case CHAIN_REFERENCE.OsmosisMainnet:
+          return 'osmosis'
+        default:
+          throw new Error(
+            `chainNamespace ${chainNamespace}, chainReference ${chainReference} not supported.`
+          )
+      }
+    // Bitcoin is not a valid asset platform: https://api.coingecko.com/api/v3/asset_platforms
+    case CHAIN_NAMESPACE.Bitcoin:
+    default:
+      throw new Error(
+        `chainNamespace ${chainNamespace}, chainReference ${chainReference} not supported.`
+      )
+  }
 }
