548194c8060986763748a87ffe8fe913a720aac7
Sean Luther <elmutt@gmail.com>
2022-06-15 10:54:14 -0600

    try catch

--- packages/swapper/src/swappers/thorchain/utils/txFeeHelpers/btcTxFees/getBtcTxFees.ts
+++ packages/swapper/src/swappers/thorchain/utils/txFeeHelpers/btcTxFees/getBtcTxFees.ts
@@ -22,32 +22,40 @@ export const getBtcTxFees = async ({
   adapterManager: ChainAdapterManager
   pubkey: string
 }): Promise<QuoteFeeData<'bip122:000000000019d6689c085ae165831e93'>> => {
-  const adapter = adapterManager.get('bip122:000000000019d6689c085ae165831e93') as
-    | bitcoin.ChainAdapter
-    | undefined
-  if (!adapter)
-    throw new SwapError(
-      `[getThorTxInfo] - No chain adapter found for ${'bip122:000000000019d6689c085ae165831e93'}.`,
-      {
-        code: SwapErrorTypes.UNSUPPORTED_CHAIN,
-        details: { chainId: 'bip122:000000000019d6689c085ae165831e93' }
+  try {
+    const adapter = adapterManager.get('bip122:000000000019d6689c085ae165831e93') as
+      | bitcoin.ChainAdapter
+      | undefined
+    if (!adapter)
+      throw new SwapError(
+        `[getBtcTxFees] - No chain adapter found for ${'bip122:000000000019d6689c085ae165831e93'}.`,
+        {
+          code: SwapErrorTypes.UNSUPPORTED_CHAIN,
+          details: { chainId: 'bip122:000000000019d6689c085ae165831e93' }
+        }
+      )
+    const feeDataOptions = await adapter.getFeeData({
+      to: vault,
+      value: sellAmount,
+      chainSpecific: { pubkey, opReturnData }
+    })
+
+    const feeData = feeDataOptions['fast']
+    const tradeFee = await estimateTradeFee(deps, buyAsset.assetId)
+
+    return {
+      fee: feeData.txFee,
+      tradeFee,
+      chainSpecific: {
+        satsPerByte: feeData.chainSpecific.satoshiPerByte,
+        byteCount: bn(feeData.txFee)
+          .dividedBy(feeData.chainSpecific.satoshiPerByte)
+          .dp(0)
+          .toString()
       }
-    )
-  const feeDataOptions = await adapter.getFeeData({
-    to: vault,
-    value: sellAmount,
-    chainSpecific: { pubkey, opReturnData }
-  })
-
-  const feeData = feeDataOptions['fast']
-  const tradeFee = await estimateTradeFee(deps, buyAsset.assetId)
-
-  return {
-    fee: feeData.txFee,
-    tradeFee,
-    chainSpecific: {
-      satsPerByte: feeData.chainSpecific.satoshiPerByte,
-      byteCount: bn(feeData.txFee).dividedBy(feeData.chainSpecific.satoshiPerByte).dp(0).toString()
     }
+  } catch (e) {
+    if (e instanceof SwapError) throw e
+    throw new SwapError('[getBtcTxFeess]', { cause: e, code: SwapErrorTypes.TRADE_QUOTE_FAILED })
   }
 }
