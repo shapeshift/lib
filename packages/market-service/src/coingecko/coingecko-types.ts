503330bdee766436c4ab99e4dd782924d2834471
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-06-27 21:56:36 +0200

    fix: optional typings

--- packages/market-service/src/coingecko/coingecko-types.ts
+++ packages/market-service/src/coingecko/coingecko-types.ts
@@ -3,11 +3,11 @@ export type CoinGeckoMarketCapNoId = Omit<CoinGeckoMarketCap, 'id'>
 export type CoinGeckoMarketData = {
   current_price: Record<string, number>
   market_cap: Record<string, number>
-  total_volume: Record<string, number>
-  high_24h: Record<string, number>
-  low_24h: Record<string, number>
+  total_volume?: Record<string, number>
+  high_24h?: Record<string, number>
+  low_24h?: Record<string, number>
   circulating_supply: number
-  total_supply: number
+  total_supply?: number
   max_supply: number
   price_change_percentage_24h: number
 }
