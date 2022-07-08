ca29e585d737e785136909d6fde17bb6ca8f1b63
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2022-07-08 08:48:17 -0600

    type token data

--- packages/unchained-client/src/evm/avalanche/parser/__tests__/mockData/tokens.ts
+++ packages/unchained-client/src/evm/avalanche/parser/__tests__/mockData/tokens.ts
@@ -1,4 +1,6 @@
-export const usdcToken = {
+import { Token } from '@shapeshiftoss/unchained-client'
+
+export const usdcToken: Token = {
   contract: '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E',
   decimals: 6,
   name: 'USD Coin',
