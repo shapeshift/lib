eaaa414042b0d58cba4dbf3ca34736c217313bbf
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-07-24 17:50:16 +1000

    feat: support all evm chains in getZrxMinMax

--- packages/chain-adapters/src/evm/index.ts
+++ packages/chain-adapters/src/evm/index.ts
@@ -1,4 +1,4 @@
-export { EvmChainIds } from './EvmBaseAdapter'
+export { EvmChainIds, isEvmChainId } from './EvmBaseAdapter'
 
 export * as ethereum from './ethereum'
 export * as avalanche from './avalanche'
