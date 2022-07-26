1a575882ff5e0e4e64030cf5e58fde046ea8b33d
Sean Luther <elmutt@gmail.com>
2022-07-26 14:20:59 -0600

    surround build trade in try catch

--- packages/swapper/src/swappers/thorchain/buildThorTrade/buildThorTrade.ts
+++ packages/swapper/src/swappers/thorchain/buildThorTrade/buildThorTrade.ts
@@ -16,92 +16,101 @@ export const buildTrade = async ({
   deps: ThorchainSwapperDeps
   input: BuildTradeInput
 }): Promise<ThorTrade<ChainId>> => {
-  const {
-    buyAsset,
-    sellAsset,
-    sellAssetAccountNumber,
-    sellAmount,
-    wallet,
-    slippage: slippageTolerance = DEFAULT_SLIPPAGE,
-    receiveAddress: destinationAddress
-  } = input
-
-  const quote = await getThorTradeQuote({ deps, input })
-  const sellAdapter = deps.adapterManager.get(sellAsset.chainId)
-
-  if (!sellAdapter)
-    throw new SwapError('[buildTrade]: unsupported sell asset', {
-      code: SwapErrorTypes.BUILD_TRADE_FAILED,
-      fn: 'buildTrade',
-      details: { sellAsset }
-    })
-  const sellAssetBip44Params = sellAdapter.buildBIP44Params({
-    accountNumber: sellAssetAccountNumber
-  })
-
-  if (input.chainId === KnownChainIds.EthereumMainnet) {
-    const ethTradeTx = await makeTradeTx({
-      wallet,
-      slippageTolerance,
-      bip44Params: sellAssetBip44Params,
-      sellAsset,
+  try {
+    const {
       buyAsset,
-      adapter: sellAdapter as unknown as ethereum.ChainAdapter,
-      sellAmount,
-      destinationAddress,
-      deps,
-      gasPrice:
-        (quote as TradeQuote<KnownChainIds.EthereumMainnet>).feeData.chainSpecific?.gasPrice ?? '0',
-      gasLimit:
-        (quote as TradeQuote<KnownChainIds.EthereumMainnet>).feeData.chainSpecific?.estimatedGas ??
-        '0',
-      tradeFee: quote.feeData.tradeFee
-    })
-
-    return {
-      chainId: KnownChainIds.EthereumMainnet,
-      ...quote,
-      receiveAddress: destinationAddress,
-      txData: ethTradeTx.txToSign
-    }
-  } else if (input.chainId === KnownChainIds.BitcoinMainnet) {
-    const { vault, opReturnData } = await getBtcThorTxInfo({
-      deps,
       sellAsset,
-      buyAsset,
+      sellAssetAccountNumber,
       sellAmount,
-      slippageTolerance: DEFAULT_SLIPPAGE,
-      destinationAddress,
       wallet,
-      bip44Params: sellAssetBip44Params,
-      accountType: input.accountType
+      slippage: slippageTolerance = DEFAULT_SLIPPAGE,
+      receiveAddress: destinationAddress
+    } = input
+
+    const quote = await getThorTradeQuote({ deps, input })
+    const sellAdapter = deps.adapterManager.get(sellAsset.chainId)
+
+    if (!sellAdapter)
+      throw new SwapError('[buildTrade]: unsupported sell asset', {
+        code: SwapErrorTypes.BUILD_TRADE_FAILED,
+        fn: 'buildTrade',
+        details: { sellAsset }
+      })
+    const sellAssetBip44Params = sellAdapter.buildBIP44Params({
+      accountNumber: sellAssetAccountNumber
     })
 
-    const buildTxResponse = await (
-      sellAdapter as unknown as bitcoin.ChainAdapter
-    ).buildSendTransaction({
-      value: sellAmount,
-      wallet,
-      to: vault,
-      chainSpecific: {
-        accountType: input.accountType,
-        satoshiPerByte: (quote as TradeQuote<KnownChainIds.BitcoinMainnet>).feeData.chainSpecific
-          .satsPerByte,
-        opReturnData
+    if (input.chainId === KnownChainIds.EthereumMainnet) {
+      const ethTradeTx = await makeTradeTx({
+        wallet,
+        slippageTolerance,
+        bip44Params: sellAssetBip44Params,
+        sellAsset,
+        buyAsset,
+        adapter: sellAdapter as unknown as ethereum.ChainAdapter,
+        sellAmount,
+        destinationAddress,
+        deps,
+        gasPrice:
+          (quote as TradeQuote<KnownChainIds.EthereumMainnet>).feeData.chainSpecific?.gasPrice ??
+          '0',
+        gasLimit:
+          (quote as TradeQuote<KnownChainIds.EthereumMainnet>).feeData.chainSpecific
+            ?.estimatedGas ?? '0',
+        tradeFee: quote.feeData.tradeFee
+      })
+
+      return {
+        chainId: KnownChainIds.EthereumMainnet,
+        ...quote,
+        receiveAddress: destinationAddress,
+        txData: ethTradeTx.txToSign
       }
-    })
+    } else if (input.chainId === KnownChainIds.BitcoinMainnet) {
+      const { vault, opReturnData } = await getBtcThorTxInfo({
+        deps,
+        sellAsset,
+        buyAsset,
+        sellAmount,
+        slippageTolerance: DEFAULT_SLIPPAGE,
+        destinationAddress,
+        wallet,
+        bip44Params: sellAssetBip44Params,
+        accountType: input.accountType
+      })
 
-    return {
-      chainId: KnownChainIds.BitcoinMainnet,
-      ...quote,
-      receiveAddress: destinationAddress,
-      txData: buildTxResponse.txToSign
+      const buildTxResponse = await (
+        sellAdapter as unknown as bitcoin.ChainAdapter
+      ).buildSendTransaction({
+        value: sellAmount,
+        wallet,
+        to: vault,
+        chainSpecific: {
+          accountType: input.accountType,
+          satoshiPerByte: (quote as TradeQuote<KnownChainIds.BitcoinMainnet>).feeData.chainSpecific
+            .satsPerByte,
+          opReturnData
+        }
+      })
+
+      return {
+        chainId: KnownChainIds.BitcoinMainnet,
+        ...quote,
+        receiveAddress: destinationAddress,
+        txData: buildTxResponse.txToSign
+      }
+    } else {
+      throw new SwapError('[buildTrade]: unsupported chain id', {
+        code: SwapErrorTypes.BUILD_TRADE_FAILED,
+        fn: 'buildTrade',
+        details: { sellAsset }
+      })
     }
-  } else {
-    throw new SwapError('[buildTrade]: unsupported chain id', {
+  } catch (e) {
+    if (e instanceof SwapError) throw e
+    throw new SwapError('[buildTrade]: error building trade', {
       code: SwapErrorTypes.BUILD_TRADE_FAILED,
-      fn: 'buildTrade',
-      details: { sellAsset }
+      fn: 'buildTrade'
     })
   }
 }
