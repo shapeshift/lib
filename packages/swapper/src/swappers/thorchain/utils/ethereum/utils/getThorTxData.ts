136ad456452277d6b8bb28a410002c16f675b641
Sean Luther <elmutt@gmail.com>
2022-08-02 12:45:05 -0600

    feat: atom support

--- packages/swapper/src/swappers/thorchain/utils/ethereum/utils/getThorTxData.ts
+++ packages/swapper/src/swappers/thorchain/utils/ethereum/utils/getThorTxData.ts
@@ -2,10 +2,7 @@ import { Asset } from '@shapeshiftoss/asset-service'
 import { fromAssetId } from '@shapeshiftoss/caip'
 
 import { SwapError, SwapErrorTypes } from '../../../../../api'
-import { bn, bnOrZero, fromBaseUnit, toBaseUnit } from '../../../../utils/bignumber'
 import { InboundResponse, ThorchainSwapperDeps } from '../../../types'
-import { getTradeRate } from '../../../utils/getTradeRate/getTradeRate'
-import { THORCHAIN_FIXED_PRECISION } from '../../constants'
 import { makeSwapMemo } from '../../makeSwapMemo/makeSwapMemo'
 import { thorService } from '../../thorService'
 import { deposit } from '../routerCalldata'
@@ -54,35 +51,16 @@ export const getThorTxInfo: GetBtcThorTxInfo = async ({
         details: { inboundAddresses }
       })
 
-    const tradeRate = await getTradeRate(sellAsset, buyAsset.assetId, sellAmount, deps)
-
-    const expectedBuyAmountPrecision8 = toBaseUnit(
-      fromBaseUnit(bnOrZero(sellAmount).times(tradeRate), sellAsset.precision),
-      THORCHAIN_FIXED_PRECISION
-    )
-
-    const tradeFeePrecision8 = toBaseUnit(bnOrZero(tradeFee), THORCHAIN_FIXED_PRECISION)
-
-    const isValidSlippageRange =
-      bnOrZero(slippageTolerance).gte(0) && bnOrZero(slippageTolerance).lte(1)
-    if (bnOrZero(expectedBuyAmountPrecision8).lt(0) || !isValidSlippageRange)
-      throw new SwapError('[makeTradeTx]: bad expected buy amount or bad slippage tolerance', {
-        code: SwapErrorTypes.BUILD_TRADE_FAILED,
-        details: { expectedBuyAmountPrecision8, slippageTolerance }
-      })
-
-    const limit = bnOrZero(expectedBuyAmountPrecision8)
-      .times(bn(1).minus(slippageTolerance))
-      .minus(bnOrZero(tradeFeePrecision8))
-      .decimalPlaces(0)
-      .toString()
-
-    const memo = makeSwapMemo({
+    const memo = await makeSwapMemo({
       buyAssetId: buyAsset.assetId,
       destinationAddress,
-      limit
+      sellAmount,
+      sellAsset,
+      buyAsset,
+      slippageTolerance,
+      deps,
+      tradeFee
     })
-
     const data = await deposit(
       router,
       vault,
--- packages/swapper/src/swappers/thorchain/utils/makeSwapMemo/makeSwapMemo.ts
+++ packages/swapper/src/swappers/thorchain/utils/makeSwapMemo/makeSwapMemo.ts
@@ -1,7 +1,15 @@
+import { Asset } from '@shapeshiftoss/asset-service'
 import { adapters } from '@shapeshiftoss/caip'
 
 import { SwapError, SwapErrorTypes } from '../../../../api'
-import { THORCHAIN_AFFILIATE_BIPS, THORCHAIN_AFFILIATE_NAME } from '../constants'
+import { bn, bnOrZero, fromBaseUnit, toBaseUnit } from '../../../utils/bignumber'
+import { ThorchainSwapperDeps } from '../../types'
+import {
+  THORCHAIN_AFFILIATE_BIPS,
+  THORCHAIN_AFFILIATE_NAME,
+  THORCHAIN_FIXED_PRECISION
+} from '../constants'
+import { getTradeRate } from '../getTradeRate/getTradeRate'
 
 // BTC (and likely other utxo coins) can only support up to 80 character memos
 const MAX_LENGTH = 80
@@ -10,15 +18,47 @@ const MAX_LENGTH = 80
  * @returns thorchain memo shortened to a max of 80 characters as described:
  * https://dev.thorchain.org/thorchain-dev/memos#mechanism-for-transaction-intent
  */
-export const makeSwapMemo = ({
+export const makeSwapMemo = async ({
   buyAssetId,
   destinationAddress,
-  limit
+  sellAsset,
+  buyAsset,
+  sellAmount,
+  deps,
+  slippageTolerance,
+  tradeFee
 }: {
   buyAssetId: string
   destinationAddress: string
-  limit: string
-}): string => {
+  sellAsset: Asset
+  buyAsset: Asset
+  sellAmount: string
+  deps: ThorchainSwapperDeps
+  slippageTolerance: string
+  tradeFee: string
+}): Promise<string> => {
+  const tradeRate = await getTradeRate(sellAsset, buyAsset.assetId, sellAmount, deps)
+  const expectedBuyAmountPrecision8 = toBaseUnit(
+    fromBaseUnit(bnOrZero(sellAmount).times(tradeRate), sellAsset.precision),
+    THORCHAIN_FIXED_PRECISION
+  )
+
+  const isValidSlippageRange =
+    bnOrZero(slippageTolerance).gte(0) && bnOrZero(slippageTolerance).lte(1)
+  if (bnOrZero(expectedBuyAmountPrecision8).lt(0) || !isValidSlippageRange)
+    throw new SwapError('[getThorTxInfo]: bad expected buy amount or bad slippage tolerance', {
+      code: SwapErrorTypes.BUILD_TRADE_FAILED,
+      details: { expectedBuyAmountPrecision8, slippageTolerance }
+    })
+
+  const tradeFeePrecision8 = toBaseUnit(bnOrZero(tradeFee), THORCHAIN_FIXED_PRECISION)
+
+  const limit = bnOrZero(expectedBuyAmountPrecision8)
+    .times(bn(1).minus(slippageTolerance))
+    .minus(bnOrZero(tradeFeePrecision8))
+    .decimalPlaces(0)
+    .toString()
+
   const thorId = adapters.assetIdToPoolAssetId({ assetId: buyAssetId })
   if (!thorId)
     throw new SwapError('[makeSwapMemo] - undefined thorId for given buyAssetId', {
