221055f8eb9ff9d31f210072546f1e37e90dc262
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2022-08-16 12:27:12 -0600

    chore: pr suggestion

--- packages/unchained-client/src/evm/ethereum/parser/yearn.ts
+++ packages/unchained-client/src/evm/ethereum/parser/yearn.ts
@@ -1,12 +1,24 @@
-import { ChainId, fromChainId, toAssetId } from '@shapeshiftoss/caip'
+import { ChainId, toAssetId } from '@shapeshiftoss/caip'
 import { ChainId as YearnChainId, Yearn } from '@yfi/sdk'
+import BigNumber from 'bignumber.js'
 import { ethers } from 'ethers'
 
 import { Tx } from '../../../generated/ethereum'
+import { BaseTxMetadata } from '../../../types'
 import { getSigHash, SubParser, TxSpecific } from '../../parser'
 import shapeShiftRouter from './abi/shapeShiftRouter'
 import yearnVault from './abi/yearnVault'
 import { SHAPE_SHIFT_ROUTER_CONTRACT } from './constants'
+
+export interface TxMetadata extends BaseTxMetadata {
+  parser: 'yearn'
+  assetId?: string
+  value?: string
+}
+
+export const isYearnTxMetadata = (data: unknown): data is TxMetadata => {
+  return (data as TxMetadata).parser === 'yearn'
+}
 
 interface ParserArgs {
   chainId: ChainId
@@ -51,6 +63,13 @@ export class Parser implements SubParser<Tx> {
 
     const txSigHash = getSigHash(tx.inputData)
 
+    const supportedSigHashes = [
+      ...Object.values(this.supportedShapeShiftFunctions),
+      ...Object.values(this.supportedYearnFunctions),
+    ]
+
+    if (!supportedSigHashes.some((hash) => hash === txSigHash)) return
+
     const abiInterface = this.getAbiInterface(txSigHash)
     if (!abiInterface) return
 
@@ -64,44 +83,37 @@ export class Parser implements SubParser<Tx> {
     // failed to decode input data
     if (!decoded) return
 
-    return (() => {
-      switch (txSigHash) {
-        case this.supportedYearnFunctions.approveSigHash:
-          if (decoded?.args._spender !== SHAPE_SHIFT_ROUTER_CONTRACT) return
-          return {
-            data: {
-              assetId: toAssetId({
-                ...fromChainId(this.chainId),
-                assetNamespace: 'erc20',
-                assetReference: tx.to,
-              }),
-              method: decoded.name,
-              parser: 'yearn',
-              value: decoded?.args._value?.toString(),
-            },
-          }
+    const data: TxMetadata = {
+      method: decoded.name,
+      parser: 'yearn',
+    }
 
-          break
-        case this.supportedShapeShiftFunctions.depositSigHash:
-          if (tx.to !== SHAPE_SHIFT_ROUTER_CONTRACT) return
-          break
-        case this.supportedYearnFunctions.withdrawSigHash:
-        case this.supportedYearnFunctions.depositSigHash:
-        case this.supportedYearnFunctions.depositAmountSigHash:
-        case this.supportedYearnFunctions.depositAmountAndRecipientSigHash:
-          if (tx.to && !this.yearnTokenVaultAddresses?.includes(tx.to)) return
-          break
-        default:
-          return
-      }
-
-      return {
-        data: {
-          method: decoded.name,
-          parser: 'yearn',
-        },
-      }
-    })()
+    switch (txSigHash) {
+      case this.supportedYearnFunctions.approveSigHash:
+        if (decoded.args._spender !== SHAPE_SHIFT_ROUTER_CONTRACT) return
+        return {
+          data: {
+            ...data,
+            assetId: toAssetId({
+              chainId: this.chainId,
+              assetNamespace: 'erc20',
+              assetReference: tx.to,
+            }),
+            value: (decoded.args._value as BigNumber).toString(),
+          },
+        }
+      case this.supportedShapeShiftFunctions.depositSigHash:
+        if (tx.to !== SHAPE_SHIFT_ROUTER_CONTRACT) return
+      // eslint-disable-next-line no-fallthrough
+      case this.supportedYearnFunctions.withdrawSigHash:
+      case this.supportedYearnFunctions.depositSigHash:
+      case this.supportedYearnFunctions.depositAmountSigHash:
+      case this.supportedYearnFunctions.depositAmountAndRecipientSigHash:
+        if (tx.to && !this.yearnTokenVaultAddresses?.includes(tx.to)) return
+      // eslint-disable-next-line no-fallthrough
+      default:
+        return { data }
+    }
   }
 
   getAbiInterface(txSigHash: string | undefined): ethers.utils.Interface | undefined {
