4fba259856e11ffa1373f0e3aecd72f9b60b0273
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-08-26 15:01:24 +0200

    feat: improve typings

--- packages/eslint-plugin-logger/src/index.ts
+++ packages/eslint-plugin-logger/src/index.ts
@@ -1,12 +1,12 @@
-import { ESLintUtils, TSESTree } from '@typescript-eslint/utils'
-import { Reference } from 'eslint-scope'
-import { CallExpression } from 'typescript'
+import {
+  BaseNode,
+  MemberExpressionNonComputedName,
+} from '@typescript-eslint/types/dist/generated/ast-spec'
+import { TSESTree } from '@typescript-eslint/utils'
+import { RuleModule } from '@typescript-eslint/utils/dist/ts-eslint'
+import { Reference, Scope } from 'eslint-scope'
 
 import { getVariableByName } from './utils'
-
-const createRule = ESLintUtils.RuleCreator(
-  () => 'https://github.com/shapeshift/lib/tree/main/packages/eslint-plugin-logger',
-)
 
 function isMemberAccess(reference: Reference) {
   const node = reference.identifier as TSESTree.Node
@@ -21,67 +21,57 @@ function isConsole(reference: Reference) {
   return id && id.name === 'console'
 }
 
-const rule = createRule({
-  name: 'no-native-console',
-  defaultOptions: [],
-  meta: {
-    fixable: 'code',
-    type: 'problem',
-    messages: {
-      noNativeConsole: 'No native console.* methods allowed, use moduleLogger.* instead',
+export const rules: Record<
+  string,
+  { meta: { type: string }; create: RuleModule<string, []>['create'] }
+> = {
+  'no-native-console': {
+    meta: {
+      type: 'problem',
     },
-    docs: {
-      description: '',
-      recommended: 'error',
+    create(context) {
+      function report(reference: Reference) {
+        const node = reference.identifier as BaseNode
+        const method = (node.parent as MemberExpressionNonComputedName).property.name
+        const consoleCallNode = node?.parent?.parent
+
+        node.loc &&
+          consoleCallNode &&
+          context.report({
+            node: consoleCallNode,
+            loc: node.loc,
+            // @ts-ignore, typescript-eslint `ReportDescriptor` TS types doesn't have a message property, but the raw eslint rules have it
+            message: `No native console.${method} allowed, use moduleLogger.${method} instead`,
+          })
+      }
+
+      return {
+        Program() {
+          const scope = context.getScope() as Scope
+          const consoleVar = getVariableByName(scope, 'console')
+          const shadowed = consoleVar && consoleVar.defs.length > 0
+
+          /*
+           * 'scope.through' includes all references to undefined
+           * variables. If the variable 'console' is not defined, it uses
+           * 'scope.through'.
+           */
+          // @ts-ignore
+          const references = consoleVar ? consoleVar.references : scope.through.filter(isConsole)
+
+          if (!shadowed) {
+            references
+              .filter(isMemberAccess)
+              .filter((reference) => {
+                const node = reference.identifier as BaseNode
+                const method = (node.parent as MemberExpressionNonComputedName).property.name
+
+                return method !== 'consoleFn' // Exclude moduleLogger itself from being reported
+              })
+              .forEach(report)
+          }
+        },
+      }
     },
-    schema: [],
   },
-  create(context) {
-    function report(reference: Reference) {
-      const node = reference.identifier as TSESTree.Node
-      // @ts-ignore FIXME
-      const consoleCallNode = node.parent.parent as CallExpression
-
-      context.report({
-        node: consoleCallNode as unknown as TSESTree.Node,
-        loc: node.loc,
-        messageId: 'noNativeConsole',
-      })
-    }
-
-    return {
-      Program() {
-        const scope = context.getScope()
-        // @ts-ignore FIXME
-        const consoleVar = getVariableByName(scope, 'console')
-        const shadowed = consoleVar && consoleVar.defs.length > 0
-
-        /*
-         * 'scope.through' includes all references to undefined
-         * variables. If the variable 'console' is not defined, it uses
-         * 'scope.through'.
-         */
-        const references = consoleVar
-          ? consoleVar.references
-          : // @ts-ignore FIXME
-            scope.through.filter(isConsole)
-
-        if (!shadowed) {
-          references
-            // @ts-ignore FIXME
-            .filter(isMemberAccess)
-            // @ts-ignore FIXME
-            .filter((reference) => {
-              const node = reference.identifier
-              const consoleMethod = node.parent.property.name
-
-              return consoleMethod !== 'consoleFn' // Exclude moduleLogger itself from being reported
-            })
-            .forEach(report)
-        }
-      },
-    }
-  },
-})
-
-export const rules = [rule]
+}
