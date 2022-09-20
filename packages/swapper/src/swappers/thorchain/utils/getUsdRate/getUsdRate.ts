b8ac1975273db76e3aca0610378402b932deee7b
Adam <asamere@gmail.com>
2022-09-20 15:41:21 -0700

    add isRune util, getUsdRate IIFE

--- packages/swapper/src/swappers/thorchain/utils/getUsdRate/getUsdRate.ts
+++ packages/swapper/src/swappers/thorchain/utils/getUsdRate/getUsdRate.ts
@@ -1,13 +1,15 @@
 import { Asset } from '@shapeshiftoss/asset-service'
-import { adapters, thorchainAssetId } from '@shapeshiftoss/caip'
+import { adapters } from '@shapeshiftoss/caip'
 
 import { SwapError, SwapErrorTypes } from '../../../../api'
 import { bn, bnOrZero } from '../../../utils/bignumber'
 import { PoolResponse, ThorchainSwapperDeps } from '../../types'
+import { isRune } from '../isRune/isRune'
 import { thorService } from '../thorService'
 
 // not sure what to do for rune usd rate - inverting USDC pool rate for now
 const usdcPool = 'ETH.USDC-0XA0B86991C6218B36C1D19D4A2E9EB0CE3606EB48'
+
 export const getUsdRate = async ({
   deps,
   input,
@@ -17,32 +19,35 @@ export const getUsdRate = async ({
 }): Promise<string> => {
   const { assetId } = input
   try {
-    const isRune = assetId === thorchainAssetId
-    let thorchainPoolId = adapters.assetIdToPoolAssetId({ assetId })
-    if (!thorchainPoolId && !isRune)
+    const thorchainPoolId: string | undefined = (() => {
+      if (isRune(assetId)) {
+        return usdcPool
+      }
+      return adapters.assetIdToPoolAssetId({ assetId })
+    })()
+
+    if (!thorchainPoolId)
       throw new SwapError(`[getUsdRate]: No thorchainPoolId found for assetId: ${assetId}`, {
         code: SwapErrorTypes.USD_RATE_FAILED,
       })
-
-    if (isRune) {
-      thorchainPoolId = usdcPool
-    }
 
     const { data: responseData } = await thorService.get<PoolResponse>(
       `${deps.midgardUrl}/pool/${thorchainPoolId}`,
     )
 
-    let rate = responseData?.assetPriceUSD
-    if (isRune) {
-      const bnRate = bnOrZero(rate)
-      if (bnRate.isZero()) {
-        throw new SwapError('[getUsdRate]: cannot invert rate zero', {
-          code: SwapErrorTypes.USD_RATE_FAILED,
-        })
+    const rate: string | undefined = (() => {
+      if (isRune(assetId)) {
+        const bnRate = bnOrZero(responseData?.assetPrice)
+        if (bnRate.isZero()) {
+          throw new SwapError('[getUsdRate]: cannot invert rate zero', {
+            code: SwapErrorTypes.USD_RATE_FAILED,
+          })
+        }
+        const inverseRate = bn(1).div(bnRate)
+        return inverseRate.toFixed(8)
       }
-      const inverseRate = bn(1).div(bnRate)
-      rate = inverseRate.toFixed(8)
-    }
+      return responseData?.assetPriceUSD
+    })()
 
     if (!rate)
       throw new SwapError(`[getUsdRate]: No rate for ${assetId}`, {
