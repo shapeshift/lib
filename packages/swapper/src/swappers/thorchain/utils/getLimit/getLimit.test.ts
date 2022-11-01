f4bfdce3378ae88d14eb1667637c21a7696ba389
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2022-11-01 16:36:22 +1100

    fix: as-is test skeleton

--- packages/swapper/src/swappers/thorchain/utils/getLimit/getLimit.test.ts
+++ packages/swapper/src/swappers/thorchain/utils/getLimit/getLimit.test.ts
@@ -1,17 +1,46 @@
+import {
+  btcAssetId,
+  btcChainId,
+  ethAssetId,
+  ethChainId,
+  thorchainAssetId,
+  thorchainChainId,
+} from '@shapeshiftoss/caip'
+import { ChainAdapterManager } from '@shapeshiftoss/chain-adapters'
+import Web3 from 'web3'
+
 import { DEFAULT_SLIPPAGE } from '../../../utils/constants'
 import { ETH, FOX, RUNE } from '../../../utils/test-data/assets'
 import { ThorchainSwapperDeps } from '../../types'
 import { getTradeRate } from '../getTradeRate/getTradeRate'
 import { getUsdRate } from '../getUsdRate/getUsdRate'
+import { mockInboundAddresses } from '../test-data/responses'
+import { thorService } from '../thorService'
 import { getLimit, GetLimitArgs } from './getLimit'
 
 jest.mock('../getUsdRate/getUsdRate')
-
+jest.mock('../thorService')
 jest.mock('../getTradeRate/getTradeRate', () => ({
   getTradeRate: jest.fn().mockReturnValue('0.000078'),
 }))
 
+const thorchainSwapperDeps: ThorchainSwapperDeps = {
+  midgardUrl: '',
+  daemonUrl: '',
+  adapterManager: new Map([
+    [thorchainChainId, { getFeeAssetId: () => thorchainAssetId }],
+    [ethChainId, { getFeeAssetId: () => ethAssetId }],
+    [btcChainId, { getFeeAssetId: () => btcAssetId }],
+  ]) as ChainAdapterManager,
+  web3: <Web3>{},
+}
+
 describe('getLimit', () => {
+  beforeEach(() => {
+    ;(thorService.get as jest.Mock<unknown>).mockReturnValue(
+      Promise.resolve({ data: mockInboundAddresses }),
+    )
+  })
   it('should get limit when buy and sell asset are both EVM-based', async () => {
     // FIXME: work out what these should return once THORChain is back online
     ;(getUsdRate as jest.Mock<unknown>).mockReturnValue(Promise.resolve('0.000078'))
@@ -20,7 +49,7 @@ describe('getLimit', () => {
       sellAsset: ETH,
       buyAssetId: FOX.assetId,
       sellAmountCryptoPrecision: '1000000000000000000',
-      deps: {} as ThorchainSwapperDeps,
+      deps: thorchainSwapperDeps,
       slippageTolerance: DEFAULT_SLIPPAGE,
       buyAssetTradeFeeUsd: '10',
       destinationAddress: '0xDef1C0ded9bec7F1a1670819833240f027b25EfF',
@@ -38,7 +67,7 @@ describe('getLimit', () => {
       sellAsset: FOX,
       buyAssetId: RUNE.assetId,
       sellAmountCryptoPrecision: '1000000000000000000',
-      deps: {} as ThorchainSwapperDeps,
+      deps: thorchainSwapperDeps,
       slippageTolerance: DEFAULT_SLIPPAGE,
       buyAssetTradeFeeUsd: '10',
       destinationAddress: '0xDef1C0ded9bec7F1a1670819833240f027b25EfF',
@@ -56,7 +85,7 @@ describe('getLimit', () => {
       sellAsset: RUNE,
       buyAssetId: FOX.assetId,
       sellAmountCryptoPrecision: '1000000000000000000',
-      deps: {} as ThorchainSwapperDeps,
+      deps: thorchainSwapperDeps,
       slippageTolerance: DEFAULT_SLIPPAGE,
       buyAssetTradeFeeUsd: '10',
       destinationAddress: '0xDef1C0ded9bec7F1a1670819833240f027b25EfF',
