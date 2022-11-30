695560f38b072dd35d0b4d189995f957430d6bd2
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-11-30 14:18:03 +0100

    feat: fetch idle asset symbols with Alchemy API

--- packages/asset-service/src/generateAssetData/ethereum/idleVaults.ts
+++ packages/asset-service/src/generateAssetData/ethereum/idleVaults.ts
@@ -1,5 +1,6 @@
 import { ethChainId as chainId, toAssetId } from '@shapeshiftoss/caip'
 import { IdleSdk, IdleVault } from '@shapeshiftoss/investor-idle'
+import axios from 'axios'
 import toLower from 'lodash/toLower'
 
 import { Asset } from '../../service/AssetService'
@@ -7,7 +8,7 @@ import { ethereum } from '../baseAssets'
 import { colorMap } from '../colorMap'
 
 // const network = 1 // 1 for mainnet
-// const provider = new JsonRpcProvider(process.env.REACT_APP_ETHEREUM_NODE_URL)
+// const provider = new JsonRpcProvider(process.env.ETHEREUM_NODE_URL)
 
 const idleSdk = new IdleSdk()
 
@@ -18,23 +19,37 @@ const explorerData = {
 }
 
 const getIdleVaults = async (): Promise<Asset[]> => {
+  const alchemyApiKey = process.env.ALCHEMY_API_KEY
   const vaults: IdleVault[] = await idleSdk.getVaults()
 
-  return vaults.map((vault: IdleVault) => {
+  const parsedVaults = []
+  for (const vault of vaults) {
+    const alchemyUrl = `https://eth-mainnet.g.alchemy.com/v2/${alchemyApiKey}`
+    const alchemyPayload = JSON.stringify({
+      jsonrpc: '2.0',
+      method: 'alchemy_getTokenMetadata',
+      params: [vault.address],
+      id: 42,
+    })
+    const { data: tokenMetadata } = await axios.post(alchemyUrl, alchemyPayload)
+    const symbol = tokenMetadata?.result?.symbol ?? vault.tokenName // Alchemy XHRs might fail, you never know
     const assetId = toAssetId({ chainId, assetNamespace: 'erc20', assetReference: vault.address })
     const displayIcon = `https://raw.githubusercontent.com/Idle-Labs/idle-dashboard/master/public/images/tokens/${vault.tokenName}.svg`
-    return {
+
+    parsedVaults.push({
       color: colorMap[assetId] ?? '#FFFFFF',
       icon: displayIcon,
       name: vault.poolName,
       precision: Number(18),
-      symbol: vault.tokenName,
+      symbol,
       tokenId: toLower(vault.address),
       chainId,
       assetId,
       ...explorerData,
-    }
-  })
+    })
+  }
+
+  return parsedVaults
 }
 
 const getUnderlyingVaultTokens = async (): Promise<Asset[]> => {
