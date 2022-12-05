db63b5117473325d3d9b0c49a220b15b7af3af13
gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-12-05 07:55:48 -0700

    fix: lint

--- packages/investor-idle/src/IdleSdk.ts
+++ packages/investor-idle/src/IdleSdk.ts
@@ -5,7 +5,7 @@ import { apiKey, baseUrl, IdleVault } from './constants'
 export class IdleSdk {
   async getVaults(): Promise<IdleVault[]> {
     const results = await axios.get(`${baseUrl}pools`, {
-      headers: { Authorization: `Bearer ${apiKey}` }
+      headers: { Authorization: `Bearer ${apiKey}` },
     })
     if (results && results.data) {
       return results.data.filter((data: IdleVault) => data.externalIntegration)
