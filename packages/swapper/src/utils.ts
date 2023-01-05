0b2cc6fda1707037dc88a96ce8700fee57f5c1b4
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-01-05 11:45:15 +1100

    perf: add caching to swapper requests

--- packages/swapper/src/utils.ts
+++ packages/swapper/src/utils.ts
@@ -1,4 +1,5 @@
 import { AssertionError } from 'assert'
+import { setupCache } from 'axios-cache-adapter'
 
 // asserts x is type doesn't work when using arrow functions
 export function assertIsDefined<T>(val: T): asserts val is NonNullable<T> {
@@ -6,3 +7,12 @@ export function assertIsDefined<T>(val: T): asserts val is NonNullable<T> {
     throw new AssertionError({ message: `Expected 'val' to be defined, but received ${val}` })
   }
 }
+
+const getRequestFilter = (cachedUrls: string[]) => (request: Request) =>
+  !cachedUrls.some((url) => request.url.includes(url))
+
+export const createCache = (maxAge: number, cachedUrls: string[]) =>
+  setupCache({
+    maxAge,
+    exclude: { filter: getRequestFilter(cachedUrls) },
+  })
