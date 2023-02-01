707a889e636acacd8574e951d4b027571ef8f674
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2023-02-01 10:14:32 -0700

    feat: add 25 bps thorchain affiliate fees

--- packages/swapper/src/swappers/thorchain/utils/constants.ts
+++ packages/swapper/src/swappers/thorchain/utils/constants.ts
@@ -4,4 +4,4 @@ export const THOR_MINIMUM_PADDING = 1.2
 export const THOR_EVM_GAS_LIMIT = '100000' // for sends of eth / erc20 into thorchain router
 export const THORCHAIN_FIXED_PRECISION = 8 // limit values are precision 8 regardless of the chain
 export const THORCHAIN_AFFILIATE_NAME = 'ss'
-export const THORCHAIN_AFFILIATE_BIPS = '0' // affiliate fee in basis points (100 = 1%)
+export const THORCHAIN_AFFILIATE_BIPS = '25' // affiliate fee in basis points (100 = 1%)
