1f06515cd4cb564949388e67d3abd6b83e29e7a7
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2023-02-20 16:58:16 -0700

    generate identicon on missing icons and regenerate asset service

--- packages/asset-service/src/generateAssetData/bsc/index.ts
+++ packages/asset-service/src/generateAssetData/bsc/index.ts
@@ -1,10 +1,25 @@
 import { bscChainId } from '@shapeshiftoss/caip'
 
+import { getRenderedIdenticonBase64 } from '../../service/GenerateAssetIcon'
 import { bsc } from '../baseAssets'
 import * as coingecko from '../coingecko'
 
 export const getAssets = async () => {
   const assets = await coingecko.getAssets(bscChainId)
   assets.push(bsc)
-  return assets
+  return assets.map((asset) => {
+    if (!asset.icon) {
+      asset.icon = getRenderedIdenticonBase64(asset.assetId, asset.symbol, {
+        identiconImage: {
+          size: 128,
+          background: [45, 55, 72, 255],
+        },
+        identiconText: {
+          symbolScale: 7,
+          enableShadow: true,
+        },
+      })
+    }
+    return asset
+  })
 }
