7508f23ec135662f1d1bb8259179dcbedf84b008
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-02-22 14:35:46 +1100

    test: assertIsValidMemo

--- packages/swapper/src/swappers/thorchain/utils/makeSwapMemo/assertIsValidMemo.test.ts
+++ packages/swapper/src/swappers/thorchain/utils/makeSwapMemo/assertIsValidMemo.test.ts
@@ -1,6 +1,6 @@
 import { bchChainId, ethChainId, thorchainChainId } from '@shapeshiftoss/caip'
 
-import { isValidMemoAddress } from './assertIsValidMemo'
+import { assertIsValidMemo, isValidMemoAddress } from './assertIsValidMemo'
 
 describe('isValidMemoAddress', () => {
   it('should validate memo address', () => {
@@ -19,7 +19,7 @@ describe('isValidMemoAddress', () => {
     expect(
       isValidMemoAddress(
         thorchainChainId,
-        'THOR.ETH',
+        'RUNE.ETH',
         'thor1j5jxvwd33rz66r88vwwwayvncjadx9jy5hvvqw',
       ),
     ).toBe(true)
@@ -53,5 +53,31 @@ describe('isValidMemoAddress', () => {
     expect(
       isValidMemoAddress(ethChainId, 'ETH.ETH', 'bc1qkw9g3tgv6m2gwc4x4hvdefcwt0uxeedfgag27h'),
     ).toBe(false)
+  })
+})
+
+describe('assertIsValidMemo', () => {
+  it('should assert memo is valid', () => {
+    expect(() =>
+      assertIsValidMemo('s:BTC.BTC:bc1qkw9g3tgv6m2gwc4x4hvdefcwt0uxeedfgag27h:420:ss:0'),
+    ).not.toThrow()
+
+    expect(() =>
+      assertIsValidMemo('s:ETH.ETH:0x8a65ac0E23F31979db06Ec62Af62b132a6dF4741:420:ss:0'),
+    ).not.toThrow()
+
+    expect(() =>
+      assertIsValidMemo('s:ETH.USDC-B48:0x8a65ac0E23F31979db06Ec62Af62b132a6dF4741:420:ss:0'),
+    ).not.toThrow()
+  })
+
+  it('should throw on invalid memo', () => {
+    expect(() =>
+      assertIsValidMemo('s:BTC.BTC:zc1qkw9g3tgv6m2gwc4x4hvdefcwt0uxeedfgag27h:420:ss:0'),
+    ).toThrow()
+
+    expect(() =>
+      assertIsValidMemo('s:RUNE.BTC:Bc1qkw9g3tgv6m2gwc4x4hvdefcwt0uxeedfgag27h:420:ss:0'),
+    ).toThrow()
   })
 })
