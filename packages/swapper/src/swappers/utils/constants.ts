c94c7fe7200df93ee7d8ebce7342224dad20eb1b
pastaghost <pastaghost@2520.io>
2023-04-06 12:19:19 -0600

    feat(swapper): add support for atom on osmosis

--- packages/swapper/src/swappers/utils/constants.ts
+++ packages/swapper/src/swappers/utils/constants.ts
@@ -1,3 +1,7 @@
+import { adapters } from '@shapeshiftoss/caip'
+
 export const APPROVAL_GAS_LIMIT = '100000' // Most approvals are around 40k, we've seen 72k in the wild, so 100000 for safety.
 export const DEFAULT_SLIPPAGE = '0.002' // .2%
 export const ALLOWABLE_MARKET_MOVEMENT = '0.01' // 1%
+
+export const atomOnOsmosisAssetId = adapters.osmosisToAssetId('ATOM') ?? ''
